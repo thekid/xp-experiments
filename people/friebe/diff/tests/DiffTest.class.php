3e1f8266d39b3960dc15299c2a88bcbda15068df
Timm Friebe <thekid@thekid.de>
2007-12-27 16:45:09 +0000

    - Add optional argument to switch on debugging - Add deletedInBetween() test
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@9974 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/friebe/diff/tests/DiffTest.class.php
+++ people/friebe/diff/tests/DiffTest.class.php
@@ -16,6 +16,30 @@
    * @purpose  Unittest
    */
   class DiffTest extends TestCase {
+    protected
+      $verbose= FALSE;
+
+    /**
+     * Constructor
+     *
+     * @param   string name
+     * @param   bool verbose default FALSE
+     */
+    public function __construct($name, $verbose= FALSE) {
+      parent::__construct($name);
+      $this->verbose= $verbose;
+    }
+    
+    /**
+     * Helper method
+     *
+     * @param   string[] a
+     * @param   string[] b
+     * @return  text.diff.operation.AbstractOperation[]
+     */
+    protected function differenceBetween($from, $to) {
+      return Difference::between($from, $to, $this->verbose);
+    }
   
     /**
      * Test
@@ -28,7 +52,7 @@
           new Copy('Hello'),
           new Copy('World')
         ), 
-        Difference::between(
+        $this->differenceBetween(
           array('Hello', 'World'),
           array('Hello', 'World')
         )
@@ -47,7 +71,7 @@
           new Insertion('cruel'),
           new Copy('World')
         ), 
-        Difference::between(
+        $this->differenceBetween(
           array('Hello', 'World'),
           array('Hello', 'cruel', 'World')
         )
@@ -66,7 +90,7 @@
           new Copy('World'),
           new Insertion('!')
         ), 
-        Difference::between(
+        $this->differenceBetween(
           array('Hello', 'World'),
           array('Hello', 'World', '!')
         )
@@ -84,7 +108,7 @@
           new Change('Hello', 'Hi'),
           new Copy('World')
         ), 
-        Difference::between(
+        $this->differenceBetween(
           array('Hello', 'World'),
           array('Hi', 'World')
         )
@@ -96,13 +120,13 @@
      *
      */
     #[@test]
-    public function changedAEnd() {
+    public function changedAtEnd() {
       $this->assertEquals(
         array(
           new Copy('Hello'),
           new Change('World', 'Master')
         ), 
-        Difference::between(
+        $this->differenceBetween(
           array('Hello', 'World'),
           array('Hello', 'Master')
         )
@@ -121,7 +145,7 @@
           new Change('World', 'Master'),
           new Copy('how are you?')
         ), 
-        Difference::between(
+        $this->differenceBetween(
           array('Hello', 'World', 'how are you?'),
           array('Hello', 'Master', 'how are you?')
         )
@@ -139,11 +163,31 @@
           new Copy('Hello'),
           new Deletion('World')
         ), 
-        Difference::between(
+        $this->differenceBetween(
           array('Hello', 'World'),
           array('Hello')
         )
       );
     }
-  }
+ 
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function deletedInBetween() {
+      $this->assertEquals(
+        array(
+          new Copy('Hello'),
+          new Copy('World'),
+          new Deletion('Hello'),
+          new Copy('World'),
+        ), 
+        $this->differenceBetween(
+          array('Hello', 'World', 'Hello', 'World'),
+          array('Hello', 'World', 'World')
+        )
+      );
+    }
+ }
 ?>
