46965f5637492b65ec2436b6a9fb47179e4ba6ee
Timm Friebe <thekid@thekid.de>
2008-01-24 09:09:30 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10053 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ people/friebe/functions/Analyze.class.php
@@ -0,0 +1,67 @@
+<?php
+/* This file is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'util.cmd.Command',
+    'io.collections.FileCollection',
+    'io.collections.iterate.FilteredIOCollectionIterator',
+    'io.collections.iterate.ExtensionEqualsFilter'
+  );
+
+  /**
+   * Analyzes sourcecode to find function calls
+   *
+   * @purpose  purpose
+   */
+  class Analyze extends Command {
+    protected
+      $scan = NULL;
+
+    /**
+     * Set directory to scan
+     *
+     * @param   string dir
+     */
+    #[@arg(position= 0)]
+    public function setDirToScan($dir) {
+      $this->scan= new FilteredIOCollectionIterator(
+        new FileCollection($dir),
+        new ExtensionEqualsFilter(xp::CLASS_FILE_EXT),
+        TRUE
+      );
+      $this->out->writeLine($this->scan);
+    }
+    
+    /**
+     * Main runner method
+     *
+     */
+    public function run() {
+      $functions= array();
+      $this->out->write('[');
+      foreach ($this->scan as $element) {
+        $t= token_get_all(file_get_contents($element->getUri()));
+        for ($i= 0, $l= 1, $s= sizeof($t); $i < $s; $i++) {
+          is_array($t) && $l+= substr_count($t[$i]{1}, "\n");
+          if (
+            T_FUNCTION !== $t[$i]{0} &&         // Omit function declarations
+            T_NEW !== $t[$i]{0} &&              // Omit instance creation
+            T_WHITESPACE === $t[$i+ 1]{0} &&    // Omit chains and member calls
+            T_STRING === $t[$i+ 2]{0} && 
+            '(' === $t[$i+ 3]{0}
+          ) {
+            // $this->out->writeLine($t[$i+ 2]{1}, '() @ ', $l, ' of ', $element);
+            $functions[$t[$i+ 2]{1}]++;
+          } 
+        }
+        $this->out->write('.');
+      }
+      $this->out->writeLine(']');
+      arsort($functions);
+      $this->out->writeLine($functions);
+    }
+  }
+?>
