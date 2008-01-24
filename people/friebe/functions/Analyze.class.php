eb1983c28c61f6ff24f66da1e8e2ff1f1ab1fec7
Timm Friebe <thekid@thekid.de>
2008-01-24 09:35:07 +0000

    - Fix bar() call not being recognized in foo(bar()) - Emit statistics
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10054 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/friebe/functions/Analyze.class.php
+++ people/friebe/functions/Analyze.class.php
@@ -32,7 +32,7 @@
         new ExtensionEqualsFilter(xp::CLASS_FILE_EXT),
         TRUE
       );
-      $this->out->writeLine($this->scan);
+      $this->out->writeLine('---> ', $this->scan);
     }
     
     /**
@@ -41,26 +41,30 @@
      */
     public function run() {
       $functions= array();
+      $total= 0;
       $this->out->write('[');
       foreach ($this->scan as $element) {
         $t= token_get_all(file_get_contents($element->getUri()));
         for ($i= 0, $l= 1, $s= sizeof($t); $i < $s; $i++) {
           is_array($t) && $l+= substr_count($t[$i]{1}, "\n");
           if (
-            T_FUNCTION !== $t[$i]{0} &&         // Omit function declarations
-            T_NEW !== $t[$i]{0} &&              // Omit instance creation
-            T_WHITESPACE === $t[$i+ 1]{0} &&    // Omit chains and member calls
+            T_FUNCTION !== $t[$i]{0} &&             // Omit function declarations
+            T_NEW !== $t[$i]{0} &&                  // Omit instance creation
+            T_OBJECT_OPERATOR !== $t[$i+ 1]{0} &&   // Omit chains and member calls
+            T_DOUBLE_COLON !== $t[$i+ 1]{0} &&      // Omit static calls
             T_STRING === $t[$i+ 2]{0} && 
             '(' === $t[$i+ 3]{0}
           ) {
             // $this->out->writeLine($t[$i+ 2]{1}, '() @ ', $l, ' of ', $element);
             $functions[$t[$i+ 2]{1}]++;
+            $total++;
           } 
         }
         $this->out->write('.');
       }
       $this->out->writeLine(']');
       arsort($functions);
+      $this->out->write('---> Found a total of ', $total, ' function calls, ', sizeof($functions), ' distinct: ');
       $this->out->writeLine($functions);
     }
   }
