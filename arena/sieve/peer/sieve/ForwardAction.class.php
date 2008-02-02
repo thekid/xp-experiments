ffce80723cfdb890efc5d6c58b2feec882596902
Timm Friebe <thekid@thekid.de>
2008-02-02 04:40:41 +0000

    - QA: Apidoc / accessors
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10115 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/sieve/peer/sieve/ForwardAction.class.php
+++ arena/sieve/peer/sieve/ForwardAction.class.php
@@ -4,16 +4,17 @@
  * $Id$ 
  */
 
-  uses('peer.sieve.Action');
+  uses('peer.sieve.Action', 'peer.mail.InternetAddress');
 
   /**
-   * (Insert class' description here)
+   * The "forward" implementation
    *
-   * @ext      extension
-   * @see      reference
-   * @purpose  purpose
+   * @see      xp://peer.sieve.Action
+   * @purpose  Action implementation
    */
   class ForwardAction extends peer·sieve·Action {
+    protected
+      $target= NULL;
 
     /**
      * Pass tags and arguments
@@ -28,5 +29,31 @@
       $this->target= $arguments[0];
     }
     
+    /**
+     * Get target address
+     *
+     * @return  string
+     */
+    public function getTarget() {
+      return $this->target;
+    }
+    
+    /**
+     * Get target address
+     *
+     * @return  peer.mail.InternetAddress
+     */
+    public function getTargetAddress() {
+      return InternetAddress::fromString($this->target);
+    }
+    
+    /**
+     * Creates a string representation of this action.
+     *
+     * @return  string
+     */
+    public function toString() {
+      return $this->getClassName().'(->'.$this->target.')';
+    }
   }
 ?>
