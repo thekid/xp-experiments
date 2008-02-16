0d3dde1b03824bea16f6488236c64019aa4c06f9
Timm Friebe <thekid@thekid.de>
2008-02-16 18:59:24 +0000

    - Initial implementation
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10169 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/kiesel/ant/net/xp_framework/quantum/task/QuantJavaTask.class.php
+++ people/kiesel/ant/net/xp_framework/quantum/task/QuantJavaTask.class.php
@@ -4,24 +4,104 @@
  * $Id$ 
  */
 
-  uses('net.xp_framework.quantum.task.QuantTask');
+  uses('net.xp_framework.quantum.task.QuantTask', 'lang.Process');
 
   /**
-   * (Insert class' description here)
+   * Executes a Java class by forking the Java VM
    *
-   * @ext      extension
-   * @see      reference
-   * @purpose  purpose
+   * @see      http://ant.apache.org/manual/CoreTasks/java.html
+   * @purpose  Task implementation
    */
   class QuantJavaTask extends QuantTask {
+    protected
+      $classpath    = NULL,
+      $classpathref = NULL,
+      $classname    = '';
 
     /**
-     * (Insert method's description here)
+     * Set class name
      *
-     * @param   
-     * @return  
+     * @param   string classname
+     */
+    #[@xmlmapping(element= '@classname')]
+    public function setClassName($classname) {
+      $this->classname= $classname;
+    }
+
+    /**
+     * Add an argument
+     *
+     * @param   string argument
+     */
+    #[@xmlmapping(element= 'arg/@value')]
+    public function addArgument($argument) {
+      $this->arguments[]= $argument;
+    }
+
+    /**
+     * Set Classpath
+     *
+     * @param   string classpath
+     */
+    #[@xmlmapping(element= '@classpath')]
+    public function setClasspath($classpath) {
+      $this->classpath= $classpath;
+    }
+
+    /**
+     * Set Classpathref
+     *
+     * @param   string classpathref
+     */
+    #[@xmlmapping(element= '@classpathref|classpath/@refid')]
+    public function setClasspathref($classpathref) {
+      $this->classpathref= $classpathref;
+    }
+
+    /**
+     * Get Classpath
+     *
+     * @param   net.xp_framework.quantum.QuantEnvironment env
+     * @return  string[]
+     */
+    public function getClasspath(QuantEnvironment $env) {
+      if ($this->classpathref) {
+        return $env->getPath($this->classpathref);
+      }
+      
+      return $env->localUri($env->substitute($this->classpath));
+    }
+
+    /**
+     * Execute this task
+     *
      */
     public function execute() {
+      $env= $this->env();
+      $cmdline= array();
+      if ($this->classpath || $this->classpathref) $cmdline[]= '-classpath "'.implode($env->pathSeparator(), $this->getClasspath($env)).'"';
+
+      // Append classname and arguments
+      $cmdline[]= $this->classname;
+      $cmdline= array_merge($cmdline, $this->arguments);
+      try {
+        
+        // Start java process
+        $p= new Process('java', $cmdline);
+        $env->out->writeLine('---> Executing '.$p->getCommandLine());
+        
+        // Read error output if any
+        while (!$p->out->eof()) {
+          $env->out->writeLine('[STDOUT:jar] '.$p->out->readLine()); 
+        }
+        // Read error output if any
+        while (!$p->err->eof()) {
+          $env->err->writeLine('[STDERR:jar] '.$p->err->readLine()); 
+        }
+        $p->close();
+      } catch (IOException $e) {
+        $env->err->writeLine($e->toString());
+      }
     }    
   }
 ?>
