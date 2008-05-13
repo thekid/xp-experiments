b121bbc749610bd06117539e6d08d943b337be66
Timm Friebe <thekid@thekid.de>
2008-05-13 14:14:34 +0000

    - Load include_path from boot.pth
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10217 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/xprt/c/xprt.c
+++ arena/xprt/c/xprt.c
@@ -5,6 +5,7 @@
 #include <libgen.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <stdio.h>
 
 #ifdef __CYGWIN__
 #include <sys/cygwin.h>
@@ -17,11 +18,13 @@ static char *cygpath(char *in) {
 #define ARG_PATH_SEPARATOR ";"
 #define ENV_PATH_SEPARATOR ":"
 #define DIR_SEPARATOR "\\"
+#define HOME_DIR getenv("HOME")
 #else
 #define PATH_TRANSLATED(s) (s)
 #define ENV_PATH_SEPARATOR ":"
 #define ARG_PATH_SEPARATOR ":"
 #define DIR_SEPARATOR "/"
+#define HOME_DIR getenv("HOME")
 #endif
 
 #define ADD_INCLUDE_XAR(inc, path, s) asprintf(                     \
@@ -31,6 +34,49 @@ static char *cygpath(char *in) {
   path,                                                             \
   s                                                                 \
 );
+
+static int add_path(char **include_path, char *dir, char *file) {
+    char *uri= NULL;
+    char path[PATH_MAX];
+    FILE *f= NULL;
+
+    /* Open URI */    
+    asprintf(&uri, "%s"DIR_SEPARATOR"%s", dir, file);
+    if (NULL == (f= fopen(uri, "rb"))) return -1;
+
+    /* Read file line by line */
+    while (NULL != fgets(path, PATH_MAX, f)) {
+        int l= strlen(path);
+
+        /* Trim trailing newline and whitespace characters */
+        while (l-- && ('\n' == path[l] || '\r' == path[l] || ' ' == path[l] || '\t' == path[l]));
+        path[l+ 1]= '\0';
+
+        /* Ignore comments and empty lines */
+        if (l < 0 || '#' == path[0]) continue;
+
+        /* Qualify path */
+        if ('~' == path[0]) {
+            char *home= HOME_DIR;
+            char *tmp= (char*) malloc(l+ strlen(home));
+            
+            strcpy(tmp, home);
+            strncat(tmp, path+ 1, l);
+            strcat(*include_path, PATH_TRANSLATED(tmp));
+        } else {
+            char *tmp= (char*) malloc(l+ strlen(dir)+ 1);
+            
+            strcpy(tmp, dir);
+            strncat(tmp, DIR_SEPARATOR, sizeof(DIR_SEPARATOR));
+            strncat(tmp, path, l+ 1);
+            strcat(*include_path, PATH_TRANSLATED(tmp));
+        }
+        strncat(*include_path, ARG_PATH_SEPARATOR, sizeof(ARG_PATH_SEPARATOR));
+    }
+    fclose(f);
+
+    return 0;
+}
 
 void execute(char *base, char *runner, char *include, int argc, char **argv) {
     char resolved[PATH_MAX];
@@ -42,17 +88,21 @@ void execute(char *base, char *runner, char *include, int argc, char **argv) {
         fprintf(stderr, "*** Cannot resolve %s\n", base);
         return;
     }
-    absolute= PATH_TRANSLATED(dirname(dirname(resolved)));
+    absolute= PATH_TRANSLATED(dirname(resolved));
 
-    /* Build include_path line */
-    asprintf(&include_path, "%s"ARG_PATH_SEPARATOR, absolute);
-    ADD_INCLUDE_XAR(include_path, absolute, "xp-rt-"XPVERSION);
-    ADD_INCLUDE_XAR(include_path, absolute, "xp-net.xp_framework-"XPVERSION);
+    /* Boot classpath */
+    include_path= strdup("");
+    if (0 != add_path(&include_path, absolute, "boot.pth")) {
+        fprintf(stderr, "*** Cannot find boot class path in %s", absolute);
+        return;
+    }
 
+    /* User classpath */
     if (include) {
         asprintf(&include_path, "%s"ARG_PATH_SEPARATOR, include);
     }
     
+    /* Always include the current directory */
     strncat(include_path, ".", sizeof("."));
 
     /* Calculate executor's filename */
@@ -82,7 +132,7 @@ void execute(char *base, char *runner, char *include, int argc, char **argv) {
     args= (char **)malloc((argc + 3) * sizeof(char *));
     args[0]= executor;
     asprintf(&args[1], "-dinclude_path=\"%s\"", include_path);
-    asprintf(&args[2], "%s"DIR_SEPARATOR"bin"DIR_SEPARATOR"%s.php", strdup(absolute), runner);
+    asprintf(&args[2], "%s"DIR_SEPARATOR"%s.php", strdup(absolute), runner);
     memcpy(args+ 3, argv, argc * sizeof(char *));
     args[argc+ 3]= NULL;
     
