ea586c766831da9d65cda8b61a6dac59c5b06139
ruben <ruben@svn.xp-framework.net>
2008-09-04 11:20:26 +0000

    - prepare common header file for oel_ct
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10353 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/wagner/oel-extension/oel_ct/src/oel/php_oel.h
+++ people/wagner/oel-extension/oel_ct/src/oel/php_oel.h
@@ -1,18 +1,8 @@
 #ifndef PHP_OEL_H
 #define PHP_OEL_H 1
 
-    #define PHP_OEL_VERSION "0.9.0"
-    #define PHP_OEL_EXTNAME "oel"
-    #define PHP_OEL_OAR_RES_NAME "oel op code array"
-    #define PHP_OEL_FUN_RES_NAME "oel function op code array"
-    #define PHP_OEL_NME_RES_NAME "oel method op code array"
-    #define PHP_OEL_AME_RES_NAME "oel abstract method op code array"
+    #include "php_oel_common.h"
 
-    #define OEL_TYPE_UNSET                 0
-    #define OEL_TYPE_OAR_BASE              1
-    #define OEL_TYPE_OAR_FUNCTION          2
-    #define OEL_TYPE_OAR_METHOD            4
-    #define OEL_TYPE_OAR_AMETHOD           5
     #define OEL_TYPE_TOKEN_CLASS           6
     #define OEL_TYPE_TOKEN_ACLASS          7
     #define OEL_TYPE_TOKEN_ICLASS          8
@@ -65,61 +55,6 @@
     #define PHP_OEL_SET_CG(o, e, p)    CG(p)= (o)->oel_cg.p; 
     #define PHP_OEL_RESAFE_CG(o, e, p) (o)->oel_cg.p= CG(p); 
     #define PHP_OEL_RESET_CG(o, e, p)  CG(p)= (e)->p; 
-
-    typedef struct _php_oel_znode {
-        znode *ext_var;
-        int    type;
-        struct _php_oel_znode *next_var;
-    } php_oel_znode;
-
-    typedef struct _php_oel_op_array {
-        php_oel_znode *stack_extvar;
-        php_oel_znode *stack_token;
-        php_oel_znode *stack_operand;
-        struct _php_oel_op_array *next;
-        struct _php_oel_op_array *child;
-        struct _php_oel_op_array *parent;
-        int final;
-        int type;
-
-        struct {
-            zend_op_array    *active_op_array;
-            zend_class_entry *active_class_entry;
-            znode             implementing_class;
-            int               interactive;
-            zend_stack        bp_stack;
-            zend_stack        switch_cond_stack;
-            zend_stack        foreach_copy_stack;
-            zend_stack        list_stack;
-            zend_llist        list_llist;
-            zend_llist        dimension_llist;
-            zend_stack        function_call_stack;
-            HashTable         *function_table;
-            HashTable         *class_table;
-        } oel_cg;
-    } php_oel_op_array;
-
-    typedef struct _php_oel_saved_env {
-        zend_bool         in_compilation;
-        uint              zend_lineno;
-        zend_op_array    *active_op_array;
-        zend_class_entry *active_class_entry;
-        znode             implementing_class;
-        int               interactive;
-        zend_stack        bp_stack;
-        zend_stack        switch_cond_stack;
-        zend_stack        foreach_copy_stack;
-        zend_stack        list_stack;
-        zend_llist        list_llist;
-        zend_llist        dimension_llist;
-        zend_stack        function_call_stack;
-        HashTable         *function_table;
-        HashTable         *class_table;
-        
-        /*
-        TODO:
-        */
-    } php_oel_saved_env;
 
     PHP_MINIT_FUNCTION(oel);
 
