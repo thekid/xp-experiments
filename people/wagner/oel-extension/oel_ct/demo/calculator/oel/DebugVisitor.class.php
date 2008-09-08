15f237f704eb3bb74ce5a1876e56235933d1c7df
ruben <ruben@svn.xp-framework.net>
2008-09-08 11:53:28 +0000

    - add oel·DebugVisitor
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10359 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ people/wagner/oel-extension/oel_ct/demo/calculator/oel/DebugVisitor.class.php
@@ -0,0 +1,46 @@
+<?php
+
+  $package= 'oel';
+
+  uses(
+    'oel.iVisitor',
+    'oel.iAcceptor'
+  );
+
+  class oel·DebugVisitor extends Object implements oel·iVisitor {
+
+    /**
+     * dump an instruction
+     *
+     * @param   oel.iAcceptor acceptor
+     * @return  mixed
+     */
+    public function visit(oel·iAcceptor $acceptor) {
+      if (!$acceptor->is_root) {
+        printf("op: %s(%s)\n", $acceptor->name, $this->paramString($acceptor->config));
+      }
+    }
+
+    /**
+     * helper to dump param strings from config array
+     *
+     * @param   array config
+     * @return  string
+     */
+    private function paramString(Array $config) {
+      $result= array();
+      foreach ($config as $conf) {
+        switch (gettype($conf)) {
+          case "integer": $result[]= sprintf("%s(%u)", gettype($conf), $conf); break;
+          case "double":  $result[]= sprintf("%s(%f)", gettype($conf), $conf); break;
+          case "string":  $result[]= sprintf("%s(\"%s\")", gettype($conf), $conf); break;
+          case "array":   $result[]= sprintf("%s(%s)", gettype($conf), $this->paramString($conf)); break;
+          case "object":  $result[]= sprintf("%s(%s)", gettype($conf), $this->paramString($conf)); break;
+          default:        $result[]= "unknown type";
+        }
+      }
+      return implode(', ', $result);
+    }
+  }
+
+?>
