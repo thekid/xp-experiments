2683d4b8a8bf3ad0334ec0a96b2c0b21a5499404
Timm Friebe <thekid@thekid.de>
2008-09-22 09:14:22 +0000

    - Recurse into xar files
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10383 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/namespaces/convert/NameMapping.class.php
+++ arena/namespaces/convert/NameMapping.class.php
@@ -12,6 +12,7 @@
     'io.collections.iterate.FilteredIOCollectionIterator',
     'io.collections.iterate.NegationOfFilter',
     'io.collections.iterate.AllOfFilter',
+    'io.collections.iterate.AnyOfFilter',
     'io.collections.iterate.CollectionFilter',
     'io.collections.iterate.NegationOfFilter',
     'io.collections.iterate.RegexFilter',
@@ -53,12 +54,50 @@
       $this->iterator= new FilteredIOCollectionIterator(
         new CollectionComposite($this->bases),
         new AllOfFilter(array(
-          new ExtensionEqualsFilter(xp::CLASS_FILE_EXT),
+          new AnyOfFilter(array(
+            new ExtensionEqualsFilter(xp::CLASS_FILE_EXT),
+            new ExtensionEqualsFilter('xar')
+          )),
           new NegationOfFilter(new RegexFilter('#'.preg_quote(DIRECTORY_SEPARATOR).'(CVS|\.svn)'.preg_quote(DIRECTORY_SEPARATOR).'#')),
           new NegationOfFilter(new CollectionFilter())
         )),
         TRUE
       );
+    }
+    
+    /**
+     * Parse classname as declared in sourcecode
+     *
+     * @param   string sourceCode
+     * @return  string name
+     */
+    protected function classNameOf($sourceCode) {
+      $t= token_get_all($sourceCode);
+      for ($i= 0, $s= sizeof($t); $i < $s; $i++) {
+        if (T_CLASS === $t[$i][0] || T_INTERFACE === $t[$i][0]) {
+          return $t[$i+ 2][1];
+        }
+      }
+      return NULL;
+    }
+    
+    /**
+     * Add a name to the name mapping
+     *
+     * @param   util.collections.HashTable<String, String> nameMap
+     * @param   string className
+     * @param   string qualifiedName
+     * @param   util.collections.Vector<String> messages
+     * @return  bool
+     */
+    protected function mapName($nameMap, $className, $qualifiedName, $messages) {
+      if ($nameMap->containsKey($className) && $nameMap[$className] != $qualifiedName) {
+        $messages->add(new String($className.' resolves to '.$nameMap[$className].' and '.$qualifiedName));
+        return FALSE;
+      } else {
+        $nameMap[$className]= new String($qualifiedName);
+        return TRUE;
+      }
     }
     
     /**
@@ -77,23 +116,37 @@
       foreach ($this->iterator as $file) {
         $uri= new String($file->getUri());
         
-        $t= token_get_all(file_get_contents($uri));
-        for ($i= 0, $s= sizeof($t); $i < $s; $i++) {
-          if (T_CLASS === $t[$i][0] || T_INTERFACE === $t[$i][0]) {
-            $className= $t[$i+ 2][1];
-            foreach ($this->bases as $base) {
-              if ($uri->startsWith($base->getUri())) $uri= $uri->substring(strlen($base->getUri()));
-            }
-            $qualifiedName= new String(str_replace('.', '::', strtr(substr($uri, 0, -strlen(xp::CLASS_FILE_EXT)), '/\\', '..')));
+        if ($uri->endsWith('.xar')) {
+          $a= new Archive(new File($uri));
+          $a->open(ARCHIVE_READ);
+          while ($entry= $a->getEntry()) {
+            if (xp::CLASS_FILE_EXT != substr($entry, -strlen(xp::CLASS_FILE_EXT))) continue;
             
-            if ($nameMap->containsKey($className)) {
-              $messages->add(new String($className.' resolves to '.$nameMap[$className].' and '.$qualifiedName));
-            } else {
-              $nameMap[$className]= $qualifiedName;
+            if (NULL === ($className= $this->classNameOf($a->extract($entry)))) {
+              $messages->add(new String('Could not determine classname from '.$entry.' in '.$uri));
+              $this->err->write('F');
+              continue;
             }
-            $this->err->write('.');
-            break;
+            
+            $qualifiedName= str_replace('/', '::', substr($entry, 0, -strlen(xp::CLASS_FILE_EXT)));
+            $this->err->write($this->mapName($nameMap, $className, $qualifiedName, $messages) ? '.' : '!');
           }
+          $a->close();
+        } else if ($uri->endsWith(xp::CLASS_FILE_EXT)) {
+          if (NULL === ($className= $this->classNameOf(file_get_contents($uri)))) {
+            $messages->add(new String('Could not determine classname from '.$uri));
+            $this->err->write('F');
+            continue;
+          }
+          foreach ($this->bases as $base) {
+            if ($uri->startsWith($base->getUri())) $uri= $uri->substring(strlen($base->getUri()));
+          }
+          $qualifiedName= str_replace('.', '::', strtr(substr($uri, 0, -strlen(xp::CLASS_FILE_EXT)), '/\\', '..'));
+          $this->err->write($this->mapName($nameMap, $className, $qualifiedName, $messages) ? '.' : '!');
+        } else {
+          $messages->add(new String('Could not determine what to do with '.$uri));
+          $this->err->writeLine('F');
+          continue;
         }
       }
 
