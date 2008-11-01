5fe51ab442402946728ea019f6b28c3a4c85e0c6
Timm Friebe <thekid@thekid.de>
2008-11-01 19:03:17 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10449 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ arena/xprt/csharp/Executor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Net.XpFramework.Runner
+{
+    class Executor
+    {
+        private static int KEY = 0;
+        private static int VALUE = 1;
+        private static char[] PATH_SEPARATOR = new char[] { Path.PathSeparator };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="base_dir"></param>
+        /// <param name="runner"></param>
+        /// <param name="tool"></param>
+        /// <param name="includes"></param>
+        /// <param name="args"></param>
+        public static int Execute(string base_dir, string runner, string tool, string[] includes, string[] args)
+        {
+            // Determine USE_XP path from either environment option or from xp.ini
+            string env = System.Environment.GetEnvironmentVariable("USE_XP");
+            IEnumerable<string> use_xp = null;
+            if (null == env)
+            {
+                if (!File.Exists(base_dir + "xp.ini"))
+                {
+                    throw new FileNotFoundException("Cannot find xp.ini in " + base_dir);
+                }
+
+                foreach (string line in File.ReadAllLines(base_dir + "xp.ini"))
+                {
+                    string[] parsed = line.Split(new char[] { '=' }, 2);
+                    if (parsed[KEY] == "use")
+                    {
+                        use_xp = PathExtension.Translate(base_dir, parsed[VALUE].Split(PATH_SEPARATOR));
+                    }
+                }
+            }
+            else
+            {
+                use_xp = PathExtension.Translate(System.Environment.CurrentDirectory, env.Split(PATH_SEPARATOR));
+            }
+            
+            // Search for tool
+            string executor = "php";
+            string argv = "-dinclude_path=\".;" + String.Join(new string(PATH_SEPARATOR), includes) + "\" -duser_dir=\"" + String.Join(";", use_xp.ToArray()) + "\" -dmagic_quotes_gpc=0";
+            foreach (string ini in PathExtension.Locate(use_xp, "php.ini", false))
+            {
+                foreach (string line in File.ReadAllLines(ini))
+                {
+                    string[] parsed = line.Split(new char[] { '=' }, 2);
+                    if (parsed[KEY] == "executor")
+                    {
+                        executor = parsed[VALUE];
+                    }
+                    else
+                    {
+                        argv += " -d" + parsed[KEY] + "=\"" + parsed[VALUE] + "\"";
+                    }
+                }
+            }
+
+            // Spawn runtime
+            var proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = executor;
+            proc.StartInfo.Arguments = argv + " \"" + PathExtension.Locate(use_xp, "tools\\" + runner + ".php", true).First() + "\" " + tool + " \"" + String.Join("\" \"", args) + "\"";
+            proc.StartInfo.UseShellExecute = false;
+            try
+            {
+                proc.Start();
+                proc.WaitForExit();
+                return proc.ExitCode;
+            }
+            finally
+            {
+                proc.Close();
+            }
+        }
+    }
+}
---
+++ arena/xprt/csharp/Xp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Net.XpFramework.Runner
+{
+    class Xp
+    {
+
+        static void Main(string[] args)
+        {
+            var tool = "";
+            var runner = "class";
+            var shift = 0;
+            var includes = new List<string>();
+
+            for (var i = 0; i < args.Length ; i++) {
+                switch (args[i])
+                {
+                    case "-v": 
+                        tool = "xp.runtime.Version"; 
+                        shift++;  
+                        break;
+
+                    case "-e": 
+                        tool = "xp.runtime.Evaluate"; 
+                        shift++; 
+                        break;
+
+                    case "-xar": 
+                        runner = "xar"; 
+                        shift++; 
+                        break;
+
+                    case "-cp":
+                        includes.Add(args[++i]);
+                        shift += 2;
+                        break;
+
+                    default:
+                        if (args[i].StartsWith("-"))
+                        {
+                            Console.Error.WriteLine("*** Invalid argument {0}", args[i]);
+                            Environment.Exit(0xFF);
+                        } 
+                        else 
+                        {
+                            i = args.Length;
+                        }
+                        break;
+                }
+            }
+
+            // Shift
+            var argv = new string[args.Length - shift];
+            System.Array.Copy(args, shift, argv, 0, args.Length - shift);
+
+            // Execute
+            try
+            {
+                Environment.Exit(Executor.Execute(PathExtension.DirName(PathExtension.Binary()), runner, tool, includes.ToArray(), argv));
+            }
+            catch (Exception e) 
+            {
+                Console.Error.WriteLine("*** " + e.Message);
+                Environment.Exit(0xFF);
+            }
+        }
+    }
+}
