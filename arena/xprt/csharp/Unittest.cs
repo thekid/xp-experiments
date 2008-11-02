33b6e9df382b18fced28dca803c21066b4346a62
Timm Friebe <thekid@thekid.de>
2008-11-02 16:08:29 +0000

    - Add other tools (xpcli xar unittest cgen doclet)
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10452 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ arena/xprt/csharp/Unittest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Net.XpFramework.Runner
+{
+    class XpCli
+    {
+
+        static void Main(string[] args)
+        {
+            // Execute
+            try
+            {
+                Environment.Exit(Executor.Execute(Paths.DirName(Paths.Binary()), "class", "xp.unittest.Runner", new string[] { }, args));
+            }
+            catch (Exception e) 
+            {
+                Console.Error.WriteLine("*** " + e.Message);
+                Environment.Exit(0xFF);
+            }
+        }
+    }
+}
