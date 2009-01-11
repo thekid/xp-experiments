b1a12f4e7b56f81b1b3b26075755354d40486a67
Timm Friebe <thekid@thekid.de>
2009-01-11 14:51:17 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10531 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ people/friebe/performance/csharp/Increment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Profiling
+{
+
+    abstract class Increment : Profileable 
+    {
+    
+        public static MethodCalls POST = new _POST(); class _POST : MethodCalls {
+            public override void run(int times) 
+            {
+                int a = 0;
+                for (int i= 0; i < times; i++) 
+                {
+                    a++;
+                }
+            }
+        }
+
+        public static MethodCalls PRE = new _PRE(); class _PRE : MethodCalls {
+            public override void run(int times) 
+            {
+                int a = 0;
+                for (int i= 0; i < times; i++) 
+                {
+                    ++a;
+                }
+            }
+        }
+
+        public static MethodCalls BINARY = new _BINARY(); class _BINARY : MethodCalls {
+            public override void run(int times) 
+            {
+                int a = 0;
+                for (int i= 0; i < times; i++) 
+                {
+                    a = a + 1;
+                }
+            }
+        }
+        
+        public abstract void run(int times);
+    }
+}
