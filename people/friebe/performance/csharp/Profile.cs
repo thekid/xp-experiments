3027cf457aa7b8b8e34a21bb381bc9e25ae6c67a
Timm Friebe <thekid@thekid.de>
2009-01-11 14:40:11 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10530 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ people/friebe/performance/csharp/Profile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Profiling
+{
+
+    class Profile 
+    {
+
+        public static void Main(string[] args)
+        {
+            try {
+                new Profile().run(args);
+            } catch (Exception e) {
+                Console.Error.WriteLine("*** " + e);
+            }
+        }
+    
+        public void run(string[] args)
+        {
+            int times = args.Length > 1 ? Convert.ToInt32(args[1]) : 10000000;
+            Type type = Type.GetType("Profiling." + args[0], true);
+
+            Console.WriteLine(
+                "== Profiling Type<{0}> ==",
+                type
+            );
+            
+            var profilees = new Dictionary<string, Profileable>();
+            foreach (var m in type.GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                profilees[m.Name]= (Profileable)m.GetValue(null);
+            }
+            
+            var watch = new Stopwatch();
+            foreach (var p in profilees) 
+            {
+                watch.Start();
+                p.Value.run(times);
+                watch.Stop();
+                
+                Console.WriteLine(
+                    "{0}: {1:0.000} seconds for {2} runs ({3:0} / second)",
+                    p.Key,
+                    watch.Elapsed.TotalSeconds,
+                    times,
+                    times * (1 / watch.Elapsed.TotalSeconds)
+                );
+            }
+            
+            Console.WriteLine(
+                "== Memory used: {0:0.000} kB ==",
+                System.GC.GetTotalMemory(true) / 1024
+            );
+        }
+    }
+}
---
+++ people/friebe/performance/csharp/Profileable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Profiling
+{
+
+    interface Profileable {
+    
+        void run(int times);
+    
+    }
+}
