a2152d6fde47f3d8c554e2c0f7267c5aeb46abfd
ruben <ruben@svn.xp-framework.net>
2009-02-27 11:45:17 +0000

    - add debug tests
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10687 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ people/wagner/oel-extension/oel_ct/testbench/tests/debug/oparray_get_finalized.php
@@ -0,0 +1,22 @@
+array(2) {
+  [0]=>
+  object(OelOpline)#1 (1) {
+    ["opcode"]=>
+    object(OelOpcode)#2 (2) {
+      ["op"]=>
+      int(62)
+      ["mne"]=>
+      string(6) "RETURN"
+    }
+  }
+  [1]=>
+  object(OelOpline)#3 (1) {
+    ["opcode"]=>
+    object(OelOpcode)#4 (2) {
+      ["op"]=>
+      int(149)
+      ["mne"]=>
+      string(16) "HANDLE_EXCEPTION"
+    }
+  }
+}
