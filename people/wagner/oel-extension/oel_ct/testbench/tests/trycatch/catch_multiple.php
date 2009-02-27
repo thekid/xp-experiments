35f38c34198df841cc11086d105cec970418041b
ruben <ruben@svn.xp-framework.net>
2009-02-27 11:46:36 +0000

    - shorten test
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10688 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/wagner/oel-extension/oel_ct/testbench/tests/trycatch/catch_multiple.php
+++ people/wagner/oel-extension/oel_ct/testbench/tests/trycatch/catch_multiple.php
@@ -1,108 +1,39 @@
 <?php
 
+  function test($e) {
+    try {
+      throw $e;
+    } catch (Dog $d) {
+      var_dump(
+        "Phaat, a flying Dog! What's your name dog?",
+        $d->getMessage()
+      );
+    } catch (Cat $c) {
+      var_dump(
+        "MMMMMMIIIIOOOUUUUU",
+        $c->getMessage()
+      );
+    } catch (Elephant $e) {
+      var_dump(
+        "Ey elephant, don't you know elephanths can't fly",
+        $e->getMessage()
+      );
+    } catch (Banana $b) {
+      var_dump(
+        "Ey, who is throwing bananas at me?",
+        $b->getMessage()
+      );
+    }
+  }
+
   class Dog extends Exception {}
   class Cat extends Exception {}
   class Elephant extends Exception {}
   class Banana extends Exception {}
 
-  $e= new Dog("Flying Dog");
-  try {
-    throw $e;
-  } catch (Dog $d) {
-    var_dump(
-      "Phaat, a flying Dog! What's your name dog?",
-      $d->getMessage()
-    );
-  } catch (Cat $c) {
-    var_dump(
-      "MMMMMMIIIIOOOUUUUU",
-      $c->getMessage()
-    );
-  } catch (Elephant $e) {
-    var_dump(
-      "Ey elephant, don't you know elephanths can't fly",
-      $e->getMessage()
-    );
-  } catch (Banana $b) {
-    var_dump(
-      "Ey, who is throwing bananas at me?",
-      $b->getMessage()
-    );
-  }
-
-  $e= new Cat("LOL, I iz in ur sourz code");
-  try {
-    throw $e;
-  } catch (Dog $d) {
-    var_dump(
-      "Phaat, a flying Dog! What's your name dog?",
-      $d->getMessage()
-    );
-  } catch (Cat $c) {
-    var_dump(
-      "MMMMMMIIIIOOOUUUUU",
-      $c->getMessage()
-    );
-  } catch (Elephant $e) {
-    var_dump(
-      "Ey elephant, don't you know elephanths can't fly",
-      $e->getMessage()
-    );
-  } catch (Banana $b) {
-    var_dump(
-      "Ey, who is throwing bananas at me?",
-      $b->getMessage()
-    );
-  }
-
-  $e= new Elephant("hm?");
-  try {
-    throw $e;
-  } catch (Dog $d) {
-    var_dump(
-      "Phaat, a flying Dog! What's your name dog?",
-      $d->getMessage()
-    );
-  } catch (Cat $c) {
-    var_dump(
-      "MMMMMMIIIIOOOUUUUU",
-      $c->getMessage()
-    );
-  } catch (Elephant $e) {
-    var_dump(
-      "Ey elephant, don't you know elephanths can't fly",
-      $e->getMessage()
-    );
-  } catch (Banana $b) {
-    var_dump(
-      "Ey, who is throwing bananas at me?",
-      $b->getMessage()
-    );
-  }
-
-  $e= new Banana("*slap*");
-  try {
-    throw $e;
-  } catch (Dog $d) {
-    var_dump(
-      "Phaat, a flying Dog! What's your name dog?",
-      $d->getMessage()
-    );
-  } catch (Cat $c) {
-    var_dump(
-      "MMMMMMIIIIOOOUUUUU",
-      $c->getMessage()
-    );
-  } catch (Elephant $e) {
-    var_dump(
-      "Ey elephant, don't you know elephanths can't fly",
-      $e->getMessage()
-    );
-  } catch (Banana $b) {
-    var_dump(
-      "Ey, who is throwing bananas at me?",
-      $b->getMessage()
-    );
-  }
+  test(new Dog("Flying Dog"));
+  test(new Cat("LOL, I iz in ur sourz code"));
+  test(new Elephant("hm?"));
+  test(new Banana("*slap*"));
 
 ?>
