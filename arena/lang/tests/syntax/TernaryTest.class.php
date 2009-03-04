0f95f965309620227fc867e8c05ccc8219d128fd
Timm Friebe <thekid@thekid.de>
2009-03-04 11:49:40 +0000

    - Add test that $a= $b ? $b : $c becomes an   AssignmentNode(a, TernaryNode(b, b, c)) instead of a   TernaryNode(AssignmentNode(a, b), b, c) # Precedence!
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10750 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/lang/tests/syntax/TernaryTest.class.php
+++ arena/lang/tests/syntax/TernaryTest.class.php
@@ -33,6 +33,27 @@
      *
      */
     #[@test]
+    public function assignment() {
+      $this->assertEquals(array(new AssignmentNode(array(
+        'position'      => array(4, 33),
+        'variable'      => $this->create(new VariableNode('$a'), array(4, 11)),
+        'expression'    => new TernaryNode(array(
+          'position'      => array(4, 33),
+          'condition'     => $this->create(new VariableNode('$argc'), array(4, 15)),
+          'expression'    => $this->create(new VariableNode('$args0'), array(4, 23)),
+          'conditional'   => new NumberNode(array('position' => array(4, 32), 'value' => '1'))
+        )),
+        'op'            => '='
+      ))), $this->parse('
+        $a= $argc ? $args0 : 1;
+      '));
+    }
+
+    /**
+     * Test ternary - expr ?: expr
+     *
+     */
+    #[@test]
     public function withoutExpression() {
       $this->assertEquals(array(new TernaryNode(array(
         'position'      => array(4, 18),
