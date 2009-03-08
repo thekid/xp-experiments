67f605ce0c6557b328282ca774bbb4127ec5e99e
Timm Friebe <thekid@thekid.de>
2009-03-08 21:19:05 +0000

    - Show more details for INIT_FCALL_BY_NAME & SEND_VAL
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10792 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/wagner/oel-extension/oel_ct/demo/io/common.inc.php
+++ people/wagner/oel-extension/oel_ct/demo/io/common.inc.php
@@ -4,7 +4,7 @@
       return var_export($this, 1);
     }
   }
-  
+
   interface Comparable {
     public function compare($a, $b);
   }
@@ -19,7 +19,9 @@
       switch ($opline->opcode->mne) {
         case 'FETCH_CLASS': $details= array($opline->op2->value); break;
         case 'DECLARE_INHERITED_CLASS': $details= array($opline->op2->value); break;
-        case 'INIT_STATIC_METHOD_CALL': array($opline->op2->value); break;
+        case 'INIT_STATIC_METHOD_CALL': $details= array($opline->op2->value); break;
+        case 'INIT_FCALL_BY_NAME': $details= array($opline->op2->value); break;
+        case 'SEND_VAL': $details= array($opline->extended_value, $opline->op1->value); break;
         default: $details= NULL; // var_dump($opline);
       }
       printf(
