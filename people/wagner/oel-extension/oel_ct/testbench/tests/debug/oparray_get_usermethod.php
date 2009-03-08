ce1a743991aba4234f2a745b8deaa28225d6394a
Timm Friebe <thekid@thekid.de>
2009-03-08 09:38:11 +0000

    - Make php tests work without ext/oel
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10776 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/wagner/oel-extension/oel_ct/testbench/tests/debug/oparray_get_usermethod.php
+++ people/wagner/oel-extension/oel_ct/testbench/tests/debug/oparray_get_usermethod.php
@@ -1,1 +1,9 @@
-<?php include(__FILE__.'.'.oel_get_zend_api_no()); ?>
+<?php 
+  // Determine ZEND_MODULE_API_NO - see ext/standard/info.c
+  ob_start();
+  phpinfo(INFO_GENERAL);
+  preg_match('/PHP Extension => ([0-9]+)/', ob_get_contents(), $matches);
+  ob_end_clean();
+  
+  include(__FILE__.'.'.$matches[1]); 
+?>
