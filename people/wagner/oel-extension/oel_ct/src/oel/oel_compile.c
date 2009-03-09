55c8569505992aed763bc7c1f5711ab3163f9ce2
Timm Friebe <thekid@thekid.de>
2009-03-09 15:57:32 +0000

    - Make this work without leaving the oel_op_array floating around in memory - Ensure a "return 1;" is at the end of the op array to be included
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10805 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/wagner/oel-extension/oel_ct/src/oel/oel_compile.c
+++ people/wagner/oel-extension/oel_ct/src/oel/oel_compile.c
@@ -15,7 +15,7 @@ static zend_op_array *oel_compile_file(zend_file_handle *file_handle, int type T
     }
 
     /* Open file */    
-	stream = php_stream_open_wrapper(filename, "rb", ENFORCE_SAFE_MODE | USE_PATH | IGNORE_URL_WIN | STREAM_OPEN_FOR_INCLUDE, &(file_handle->opened_path));
+    stream = php_stream_open_wrapper(filename, "rb", ENFORCE_SAFE_MODE | USE_PATH | IGNORE_URL_WIN | STREAM_OPEN_FOR_INCLUDE, &(file_handle->opened_path));
     if (!stream) {
         return NULL;
     }
@@ -29,22 +29,51 @@ static zend_op_array *oel_compile_file(zend_file_handle *file_handle, int type T
         php_stream_close(stream);
         return op_array;
     } else {
+        zend_op_array *op_array = (zend_op_array *) emalloc(sizeof(zend_op_array));
+        zend_op_array *original_oel_op_array;
+    
         php_oel_op_array *res_op_array;
         unsigned char     buf[8];
         zend_class_entry  *current_ce;
 
+        init_op_array(op_array, ZEND_USER_FUNCTION, INITIAL_OP_ARRAY_SIZE TSRMLS_CC);
+
+        /* Create OEL op array, but replace op_array by the one created here */
         res_op_array= oel_create_new_op_array(TSRMLS_C);
+        original_oel_op_array = res_op_array->oel_cg.active_op_array; 
+        res_op_array->oel_cg.active_op_array = op_array;
         current_ce = NULL;
         
-        /* Unserialize op array, finalize it */
+        /* Unserialize op array */
         unserialize_oel_op_array(&res_op_array, current_ce, buf, stream TSRMLS_CC);
+        php_stream_close(stream);
         
-        /* Prepare for execution */
+        if (NULL == res_op_array) {
+            return NULL;
+        }
+        
+        /* Merge class and function table */
         zend_hash_merge(EG(function_table), res_op_array->oel_cg.function_table, NULL, NULL, sizeof(zend_function),    0);
         zend_hash_merge(EG(class_table),    res_op_array->oel_cg.class_table,    NULL, NULL, sizeof(zend_class_entry), 0);
+        
+        /* Free OEL op array */
+        res_op_array->oel_cg.active_op_array = original_oel_op_array;
+        php_oel_destroy_op_array(res_op_array TSRMLS_CC);
+        
+        /* Verify consistency - every opcode array ends with RETURN @-2, HANDLE_EXCEPTION @-1 */
+        if (op_array->last < 2 || op_array->opcodes[op_array->last- 2].opcode != ZEND_RETURN) {
+            zend_error(E_COMPILE_ERROR, "Malformed opcode array read");
+            return NULL;
+        }
 
-        php_stream_close(stream);
-        return res_op_array->oel_cg.active_op_array;
+        /* Create a return 1; */
+        op_array->opcodes[op_array->last- 2].op1.op_type = IS_CONST;
+        op_array->opcodes[op_array->last- 2].op1.u.constant.type = IS_LONG;
+        op_array->opcodes[op_array->last- 2].op1.u.constant.value.lval = 1;
+        op_array->opcodes[op_array->last- 2].op1.u.constant.is_ref = 0;
+        op_array->opcodes[op_array->last- 2].op1.u.constant.refcount = 1;
+        
+        return op_array;
     }
 }
 /* }}} */
