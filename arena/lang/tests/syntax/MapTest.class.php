607aed6c59846419b89bc45d2cbb9d5e2b54b1a1
Timm Friebe <thekid@thekid.de>
2009-03-17 10:32:22 +0000

    - Change syntax for maps from "{ key => value }" to "[ key : value ]" # Inspired by http://groovy.codehaus.org/Collections#Collections-Maps
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10850 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/lang/tests/syntax/MapTest.class.php
+++ arena/lang/tests/syntax/MapTest.class.php
@@ -4,32 +4,14 @@
  * $Id$
  */
 
-  uses(
-    'unittest.TestCase',
-    'xp.compiler.Lexer',
-    'xp.compiler.Parser'
-  );
+  uses('tests.syntax.ParserTestCase');
 
   /**
    * TestCase
    *
    */
-  class MapTest extends TestCase {
+  class MapTest extends ParserTestCase {
   
-    /**
-     * Parse method source and return statements inside this method.
-     *
-     * @param   string src
-     * @return  xp.compiler.Node[]
-     */
-    protected function parse($src) {
-      return create(new Parser())->parse(new xp·compiler·Lexer('class Container {
-        public void method() {
-          '.$src.'
-        }
-      }', '<string:'.$this->name.'>'))->declaration->body['methods'][0]->body;
-    }
-
     /**
      * Test an empty untyped map
      *
@@ -37,11 +19,11 @@
     #[@test]
     public function emptyUntypedMap() {
       $this->assertEquals(array(new MapNode(array(
-        'position'      => array(4, 12),
+        'position'      => array(4, 11),
         'elements'      => NULL,
         'type'          => NULL,
       ))), $this->parse('
-        {};
+        [:];
       '));
     }
 
@@ -52,24 +34,24 @@
     #[@test]
     public function untypedMap() {
       $this->assertEquals(array(new MapNode(array(
-        'position'      => array(4, 44),
+        'position'      => array(4, 11),
         'elements'      => array(
           array(
             new NumberNode(array('position' => array(4, 13), 'value' => '1')),
-            new StringNode(array('position' => array(4, 18), 'value' => 'one')),
+            new StringNode(array('position' => array(4, 17), 'value' => 'one')),
           ),
           array(
-            new NumberNode(array('position' => array(4, 23), 'value' => '2')),
-            new StringNode(array('position' => array(4, 28), 'value' => 'two')),
+            new NumberNode(array('position' => array(4, 22), 'value' => '2')),
+            new StringNode(array('position' => array(4, 26), 'value' => 'two')),
           ),
           array(
-            new NumberNode(array('position' => array(4, 33), 'value' => '3')),
-            new StringNode(array('position' => array(4, 38), 'value' => 'three')),
+            new NumberNode(array('position' => array(4, 31), 'value' => '3')),
+            new StringNode(array('position' => array(4, 35), 'value' => 'three')),
           ),
         ),
         'type'          => NULL,
       ))), $this->parse('
-        { 1 => "one", 2 => "two", 3 => "three" };
+        [ 1 : "one", 2 : "two", 3 : "three" ];
       '));
     }
   }
