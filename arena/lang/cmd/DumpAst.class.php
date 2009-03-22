234e68067beae7ad42659e6107aeb45673d87408
Timm Friebe <thekid@thekid.de>
2009-03-22 12:31:39 +0000

    - Detect syntax depending on file extension
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10875 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/lang/cmd/DumpAst.class.php
+++ arena/lang/cmd/DumpAst.class.php
@@ -3,23 +3,43 @@
  *
  * $Id$
  */
+
   uses(
     'util.cmd.Command',
     'io.File',
-    'io.FileUtil',
-    'xp.compiler.syntax.xp.Lexer',
-    'xp.compiler.syntax.xp.Parser'
+    'io.streams.FileInputStream'
   );
 
   /**
    * Dumps abstract syntax tree (AST)
    *
-   * @see      xp://xp.compiler.Parser
    * @purpose  Utility
    */
   class DumpAst extends Command {
     protected
-      $in= NULL;
+      $in       = NULL,
+      $syntax   = NULL;
+
+    /**
+     * Returns a parser object
+     *
+     * @param   lang.reflect.Package p
+     * @return  text.parser.generic.AbstractParser
+     */
+    protected function parser(Package $p) {
+      return $p->loadClass('Parser')->newInstance();
+    }
+
+    /**
+     * Returns a lexer object
+     *
+     * @param   lang.reflect.Package p
+     * @param   io.File in
+     * @return  text.parser.generic.AbstractLexer
+     */
+    protected function lexer(Package $p, File $in) {
+      return $p->loadClass('Lexer')->newInstance(new FileInputStream($in), $in->getURI());
+    }
       
     /**
      * Set file to parse
@@ -29,6 +49,7 @@
     #[@arg(position= 0)]
     public function setIn($in) {
       $this->in= new File($in);
+      $this->syntax= Package::forName('xp.compiler.syntax')->getPackage($this->in->getExtension());
     }
 
     /**
@@ -37,14 +58,22 @@
      */
     public function run() {
       try {
-        $ast= create(new xp·compiler·syntax·xp·Parser())->parse(new xp·compiler·syntax·xp·Lexer(
-          FileUtil::getContents($this->in),
-          $this->in->getURI()
-        ));
+        $ast= create($this->parser($this->syntax))->parse($this->lexer($this->syntax, $this->in));
+      } catch (ClassNotFoundException $e) {
+        $this->err->writeLinef(
+          '*** Cannot parse "%s" files: %s', 
+          $this->in->getExtension(),
+          $e->compoundMessage()
+        );
+        return;
       } catch (ParseException $e) {
-        $this->err->writeLine('*** Parse error: ', $e->getCause());
+        $this->err->writeLinef(
+          '*** Parse error: %s', 
+          $e->getCause()->compoundMessage()
+        );
         return;
       }
+
       $this->out->writeLine($ast);
     }
   }
