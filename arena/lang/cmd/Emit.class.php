ccbff46423833f5311969512842428c5069b9015
Timm Friebe <thekid@thekid.de>
2009-03-22 11:20:43 +0000

    - Detect syntax depending on file extension
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10871 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/lang/cmd/Emit.class.php
+++ arena/lang/cmd/Emit.class.php
@@ -1,27 +1,46 @@
 <?php
 /* This file is part of the XP framework
  *
- * $Id: DumpAst.class.php 10023 2008-01-15 18:32:50Z friebe $
+ * $Id$
  */
+
   uses(
     'util.cmd.Command',
     'io.File',
-    'io.FileUtil',
-    'xp.compiler.syntax.xp.Lexer',
-    'xp.compiler.syntax.xp.Parser',
-    'xp.compiler.emit.oel.Emitter'
+    'io.streams.FileInputStream'
   );
 
   /**
    * Emits code
    *
-   * @see      xp://xp.compiler.Parser
    * @purpose  Utility
    */
   class Emit extends Command {
     protected
       $in       = NULL,
-      $emitter = NULL;
+      $emitter  = NULL,
+      $syntax   = NULL;
+    
+    /**
+     * Returns a parser object
+     *
+     * @param   lang.reflect.Package p
+     * @return  text.parser.generic.AbstractParser
+     */
+    protected function parser(Package $p) {
+      return $p->loadClass('Parser')->newInstance();
+    }
+
+    /**
+     * Returns a lexer object
+     *
+     * @param   lang.reflect.Package p
+     * @param   io.File in
+     * @return  text.parser.generic.AbstractLexer
+     */
+    protected function lexer(Package $p, File $in) {
+      return $p->loadClass('Lexer')->newInstance(new FileInputStream($in), $in->getURI());
+    }
       
     /**
      * Set file to parse
@@ -31,10 +50,11 @@
     #[@arg(position= 0)]
     public function setIn($in) {
       $this->in= new File($in);
+      $this->syntax= Package::forName('xp.compiler.syntax')->getPackage($this->in->getExtension());
     }
     
     /**
-     * (Insert method's description here)
+     * Sets emitter (default: "oel")
      *
      * @param   string emitter
      */
@@ -48,37 +68,34 @@
     }
     
     /**
-     * (Insert method's description here)
-     *
-     * @param   
-     * @return  
-     */
-    #[@arg]
-    public function setArgs($args= NULL) {
-      $this->args= NULL !== $args ? explode(',', $args) : array();
-    }
-
-    /**
      * Main runner method
      *
      */
     public function run() {
       try {
-        $ast= create(new xp·compiler·syntax·xp·Parser())->parse(new xp·compiler·syntax·xp·Lexer(
-          FileUtil::getContents($this->in),
-          $this->in->getURI()
-        ));
+        $ast= create($this->parser($this->syntax))->parse($this->lexer($this->syntax, $this->in));
+      } catch (ClassNotFoundException $e) {
+        $this->err->writeLinef(
+          '*** Cannot parse "%s" files: %s', 
+          $this->in->getExtension(),
+          $e->compoundMessage()
+        );
+        return;
       } catch (ParseException $e) {
-        $this->err->writeLine('*** ', $e);
+        $this->err->writeLinef(
+          '*** Parse error: %s', 
+          $e->getCause()->compoundMessage()
+        );
         return;
       }
+
       try {
         $file= $this->emitter->emit($ast);
       } catch (Throwable $t) {
         $this->err->writeLine('*** ', $t);
         return;
       }
-      $this->out->writeLine('===> Wrote ', $file);
+      $this->out->writeLine('===> ', $this->emitter->getClassName(), ' wrote ', $file);
     }
   }
 ?>
