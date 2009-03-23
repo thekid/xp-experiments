ddc1e4ac4e8ce06f832fe4fee8dab26a6c883ab7
Timm Friebe <thekid@thekid.de>
2009-03-23 22:27:49 +0000

    - Add casting tests for int, double, array, string - Add invocationWithConstantArg ("init(TRUE);") test # Currently fails ^^^
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10887 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/lang/tests/syntax/php/CastingTest.class.php
+++ arena/lang/tests/syntax/php/CastingTest.class.php
@@ -31,6 +31,78 @@
     }
 
     /**
+     * Test a string-cast
+     *
+     */
+    #[@test]
+    public function stringCast() {
+      $this->assertEquals(array($this->create(new AssignmentNode(array(
+        'variable'      => $this->create(new VariableNode('$a'), array(4, 11)),
+        'expression'    => $this->create(new CastNode(array(
+          'type'          => new TypeName('string'),
+          'expression'    => $this->create(new NumberNode(array('value' => '1')), array(4, 23))
+        )), array(4, 24)),
+        'op'            => '=',
+      )), array(4, 24))), $this->parse('
+        $a= (string)1;
+      '));
+    }
+
+    /**
+     * Test an array-cast
+     *
+     */
+    #[@test]
+    public function arrayCast() {
+      $this->assertEquals(array($this->create(new AssignmentNode(array(
+        'variable'      => $this->create(new VariableNode('$a'), array(4, 11)),
+        'expression'    => $this->create(new CastNode(array(
+          'type'          => new TypeName('var[]'),
+          'expression'    => $this->create(new NumberNode(array('value' => '1')), array(4, 22))
+        )), array(4, 23)),
+        'op'            => '=',
+      )), array(4, 23))), $this->parse('
+        $a= (array)1;
+      '));
+    }
+
+    /**
+     * Test an int-cast
+     *
+     */
+    #[@test]
+    public function intCast() {
+      $this->assertEquals(array($this->create(new AssignmentNode(array(
+        'variable'      => $this->create(new VariableNode('$a'), array(4, 11)),
+        'expression'    => $this->create(new CastNode(array(
+          'type'          => new TypeName('int'),
+          'expression'    => $this->create(new NumberNode(array('value' => '1')), array(4, 20))
+        )), array(4, 21)),
+        'op'            => '=',
+      )), array(4, 21))), $this->parse('
+        $a= (int)1;
+      '));
+    }
+
+    /**
+     * Test an double-cast
+     *
+     */
+    #[@test]
+    public function doubleCast() {
+      $this->assertEquals(array($this->create(new AssignmentNode(array(
+        'variable'      => $this->create(new VariableNode('$a'), array(4, 11)),
+        'expression'    => $this->create(new CastNode(array(
+          'type'          => new TypeName('double'),
+          'expression'    => $this->create(new NumberNode(array('value' => '1')), array(4, 23))
+        )), array(4, 24)),
+        'op'            => '=',
+      )), array(4, 24))), $this->parse('
+        $a= (double)1;
+      '));
+    }
+
+    /**
      * Test a casting a variabke
      *
      */
@@ -45,6 +117,24 @@
         'op'            => '=',
       )), array(4, 23))), $this->parse('
         $a= (bool)$v;
+      '));
+    }
+
+    /**
+     * Test an invocation with a constants as argument is not confused with a cast
+     *
+     */
+    #[@test]
+    public function invocationWithConstantArg() {
+      $this->assertEquals(array($this->create(new InvocationNode(array(
+        'name'          => 'init',
+        'parameters'    => array(
+          $this->create(new ConstantNode(array(
+            'value'         => 'TRUE',
+          )), array(4, 20))
+        ),
+      )), array(4, 15))), $this->parse('
+        init(TRUE);
       '));
     }
 
