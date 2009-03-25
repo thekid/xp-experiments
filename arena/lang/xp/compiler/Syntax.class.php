04dcfa5b46aebb67a98ccd5789177df80e8f42c5
Timm Friebe <thekid@thekid.de>
2009-03-25 17:41:31 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@10912 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ arena/lang/xp/compiler/Syntax.class.php
@@ -0,0 +1,79 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Syntax base class
+   *
+   * @purpose  Abstract base class
+   */
+  abstract class Syntax extends Object {
+    private static $syntaxes= array();
+    protected $parser= NULL;
+    
+    static function __static() {
+      foreach (Package::forName('xp.compiler.syntax')->getPackages() as $syntax) {
+        self::$syntaxes[$syntax->getSimpleName()]= $syntax->loadClass('Syntax')->newInstance();
+      }
+    }
+    
+    /**
+     * Constructor
+     *
+     */
+    public function __construct() {
+      $this->parser= $this->newParser();
+    }
+    
+    /**
+     * Retrieve a syntax for a given name
+     *
+     * @param   string syntax
+     * @return  xp.compiler.syntax.Compiler
+     */
+    public static function forName($syntax) {
+      if (!isset(self::$syntaxes[$syntax])) {
+        throw new IllegalArgumentException('Syntax "'.$syntax.'" not supported');
+      }
+      return self::$syntaxes[$syntax];
+    }
+    
+    /**
+     * Parse
+     *
+     * @param   io.streams.InputStream in
+     * @param   string source default NULL
+     * @return  xp.compiler.ast.ParseTree tree
+     */
+    public function parse(InputStream $in, $source= NULL) {
+      return $this->parser->parse($this->newLexer($in, $source ? $source : $in->toString()));
+    }
+    
+    /**
+     * Creates a string representation
+     *
+     * @return  string
+     */
+    public function toString() {
+      return $this->getClassName().'('.$this->hashCode().')';
+    }
+    
+    /**
+     * Creates a parser instance
+     *
+     * @return  text.parser.generic.AbstractParser
+     */
+    protected abstract function newParser();
+
+    /**
+     * Creates a lexer instance
+     *
+     * @param   io.streams.InputStream in
+     * @param   string source
+     * @return  text.parser.generic.AbstractLexer
+     */
+    protected abstract function newLexer(InputStream $in, $source);
+  }
+?>
