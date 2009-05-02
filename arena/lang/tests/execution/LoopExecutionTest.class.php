192011ae7c1bf5b9e24dcb34579575393fca9fd6
Timm Friebe <thekid@thekid.de>
2009-05-02 12:22:58 +0000

    - Add tests for:   . while   . do...while   . for
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11030 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/lang/tests/execution/LoopExecutionTest.class.php
+++ arena/lang/tests/execution/LoopExecutionTest.class.php
@@ -35,5 +35,73 @@
     public function emptyForeachLoop() {
       $this->assertNull($this->run('foreach ($arg in [1, 2, 3]) { }'));
     }
+
+    /**
+     * Test while
+     *
+     */
+    #[@test]
+    public function whileLoop() {
+      $this->assertEquals(array(1, 2, 3), $this->run('
+        $r= []; $s= 1;
+        while ($s <= 3) { $r[]= $s++; } 
+        return $r;
+      '));
+    }
+
+    /**
+     * Test while
+     *
+     */
+    #[@test]
+    public function emptyWhileLoop() {
+      $this->assertEquals(-1, $this->run('$s= 3; while ($s--) { } return $s;'));
+    }
+
+    /**
+     * Test do
+     *
+     */
+    #[@test]
+    public function doLoop() {
+      $this->assertEquals(array(1, 2, 3), $this->run('
+        $r= []; $s= 1;
+        do { $r[]= $s++; } while ($s < 4)
+        return $r;
+      '));
+    }
+
+    /**
+     * Test do
+     *
+     */
+    #[@test]
+    public function emptydoLoop() {
+      $this->assertEquals(-1, $this->run('$s= 3; do { } while ($s--) return $s;'));
+    }
+
+    /**
+     * Test for
+     *
+     */
+    #[@test]
+    public function forLoop() {
+      $this->assertEquals(array(1, 2, 3), $this->run('
+        $r= [];
+        for ($s= 1; $s <= 3; $s++) {
+          $r[]= $s;
+        }
+        return $r;
+      '));
+    }
+
+    /**
+     * Test for
+     *
+     */
+    #[@test]
+    public function emptyForLoop() {
+      $this->assertEquals(0, $this->run('for ($s= 3; $s; $s--) { } return $s;'));
+    }
   }
 ?>
