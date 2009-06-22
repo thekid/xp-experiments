21314c8463835b9bfd0b7f2895f329ea682f988e
Timm Friebe <thekid@thekid.de>
2009-06-22 17:53:16 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11158 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ arena/lang/tests/types/ScopeTest.class.php
@@ -0,0 +1,131 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'unittest.TestCase',
+    'xp.compiler.types.Scope',
+    'xp.compiler.ast.VariableNode'
+  );
+
+  /**
+   * TestCase
+   *
+   * @see      xp://xp.compiler.types.Scope
+   */
+  class ScopeTest extends TestCase {
+    protected $fixture= NULL;
+    
+    /**
+     * Sets up this testcase
+     *
+     */
+    public function setUp() {
+      $this->fixture= new Scope();
+    }
+    
+    /**
+     * Test typeOf() method
+     *
+     */
+    #[@test]
+    public function arrayType() {
+      $this->assertEquals(new TypeName('var[]'), $this->fixture->typeOf(new ArrayNode()));
+    }
+    
+    /**
+     * Test typeOf() method
+     *
+     */
+    #[@test]
+    public function mapType() {
+      $this->assertEquals(new TypeName('[var:var]'), $this->fixture->typeOf(new MapNode()));
+    }
+    
+    /**
+     * Test typeOf() method
+     *
+     */
+    #[@test]
+    public function stringType() {
+      $this->assertEquals(new TypeName('string'), $this->fixture->typeOf(new StringNode()));
+    }
+    
+    /**
+     * Test typeOf() method
+     *
+     */
+    #[@test]
+    public function intType() {
+      $this->assertEquals(new TypeName('int'), $this->fixture->typeOf(new IntegerNode()));
+    }
+    
+    /**
+     * Test typeOf() method
+     *
+     */
+    #[@test]
+    public function hexType() {
+      $this->assertEquals(new TypeName('int'), $this->fixture->typeOf(new HexNode()));
+    }
+    
+    /**
+     * Test typeOf() method
+     *
+     */
+    #[@test]
+    public function decimalType() {
+      $this->assertEquals(new TypeName('double'), $this->fixture->typeOf(new DecimalNode()));
+    }
+    
+    /**
+     * Test typeOf() method
+     *
+     */
+    #[@test]
+    public function nullType() {
+      $this->assertEquals(new TypeName('lang.Object'), $this->fixture->typeOf(new NullNode()));
+    }
+    
+    /**
+     * Test typeOf() method
+     *
+     */
+    #[@test]
+    public function boolType() {
+      $this->assertEquals(new TypeName('bool'), $this->fixture->typeOf(new BooleanNode()));
+    }
+    
+    /**
+     * Test typeOf() method
+     *
+     */
+    #[@test]
+    public function typeOfAComparison() {
+      $this->assertEquals(new TypeName('bool'), $this->fixture->typeOf(new ComparisonNode()));
+    }
+
+    /**
+     * Test setType() and typeOf() methods
+     *
+     */
+    #[@test]
+    public function registeredType() {
+      with ($v= new VariableNode('h'), $t= new TypeName('util.collections.HashTable')); {
+        $this->fixture->setType($v, $t);
+        $this->assertEquals($t, $this->fixture->typeOf($v));
+      }
+    }
+
+    /**
+     * Test typeOf() method
+     *
+     */
+    #[@test]
+    public function unknownType() {
+      $this->assertEquals(TypeName::$VAR, $this->fixture->typeOf(new VariableNode('v')));
+    }
+  }
+?>
