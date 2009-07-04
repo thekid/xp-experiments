7b2383c602400c98ac72bda04552884dd3d08233
Timm Friebe <thekid@thekid.de>
2009-07-04 19:38:55 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11202 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ arena/lang/xp/compiler/types/Constructor.class.php
@@ -0,0 +1,29 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  $package= 'xp.compiler.types';
+
+  /**
+   * Represents a constructor
+   *
+   * @see      xp://xp.compiler.types.Types
+   */
+  class xp·compiler·types·Constructor extends Object {
+    public
+      $modifiers  = 0,
+      $parameters = array(),
+      $holder     = NULL;
+
+    /**
+     * Constructor
+     *
+     * @param   string name
+     */
+    public function __construct($name= '') {
+      $this->name= $name;
+    }
+  }
+?>
