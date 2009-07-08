265ed1861be434775dfda4ca645f64e4ddbd9a1d
Timm Friebe <thekid@thekid.de>
2009-07-08 16:22:18 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11245 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ people/friebe/ragent/src/de/thekid/exchange/ragent/Answer.class.php
@@ -0,0 +1,74 @@
+<?php
+/* This file is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'util.cmd.Command',
+    'peer.mail.store.ImapStore',
+    'peer.mail.store.NullStoreCache'
+  );
+
+  /**
+   * (Insert class' description here)
+   *
+   */
+  class Answer extends Command {
+    protected $store= NULL;
+    
+    const CONTENT_CLASS_CALENDARMESSAGE = 'urn:content-classes:calendarmessage';
+  
+    /**
+     * Set store DSN
+     *
+     * @param   string dsn
+     */
+    #[@arg]
+    public function setStore($dsn) {
+      $this->store= new ImapStore(new NullStoreCache());
+      $this->store->connect($dsn);
+    }
+
+    /**
+     * Main runner method
+     *
+     */
+    public function run() {
+      with ($inbox= $this->store->getFolder('INBOX')); {
+        $inbox->open();
+        
+        while ($msg= $inbox->getMessage()) {
+          switch ($msg->getHeader('Content-class')) {
+            case self::CONTENT_CLASS_CALENDARMESSAGE: {
+              $this->out->writeLine('Message ', $msg);
+              while ($part= $msg->getPart()) {
+                if ('text/calendar' != $part->getContentType()) continue;
+                
+                $this->out->writeLine('-> Calendar ', $part);
+                
+                // TODO: Check if we're free & answer this request
+                break;
+              }
+              break;
+            }
+            
+            default: {
+              // $this->err->writeLine('*** Unknown: ', $msg);
+            }
+          }
+        }
+        
+        $inbox->close();
+      }
+    }
+    
+    /**
+     * Destructor. Ensures connection is closed
+     *
+     */
+    public function __destruct() {
+      $this->store && $this->store->close();
+    }
+  }
+?>
