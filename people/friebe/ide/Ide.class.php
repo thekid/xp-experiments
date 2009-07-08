b40cbc4e0597357e16f2ea24b546621fe6b0207f
Timm Friebe <thekid@thekid.de>
2009-07-08 10:04:23 +0000

    - Separate from nedit
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11234 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/friebe/ide/Ide.class.php
+++ people/friebe/ide/Ide.class.php
@@ -2,11 +2,16 @@
   uses(
     'org.gnome.glade.GladeSource',
     'org.gnome.gtk.Window',
+    'org.gnome.gtk.Button',
     'lang.Process',
     'io.File',
+    'util.MimeType',
+    'util.collections.HashTable',
     'lang.archive.Archive',
     'lang.types.String',
+    'lang.types.Integer',
     'text.StreamTokenizer',
+    'io.streams.Streams',
     'io.collections.FileCollection',
     'io.collections.ArchiveCollection',
     'io.collections.iterate.FilteredIOCollectionIterator',
@@ -19,6 +24,7 @@
   class Ide extends Object {
     protected $pixbufs= array();
     protected $server= NULL;
+    protected $files= NULL;
 
     public function loadProjects($base) {
       $this->projects->clear();
@@ -32,12 +38,6 @@
           new Project($origin))
         );
       }
-    }
-    
-    public function editorCommand() {
-      $args= func_get_args();
-      $client= new Process('nedit-nc', array_merge(array('-svrname '.__CLASS__), $args));
-      $client->close();
     }
     
     public function onComplete($entry, $base) {
@@ -73,12 +73,29 @@
             $packages= array();
             foreach (new FilteredIOCollectionIterator($collection, new ExtensionEqualsFilter(xp::CLASS_FILE_EXT), TRUE) as $class) {
               $qname= strtr(substr($class->getURI(), strlen($collection->getURI()), -strlen(xp::CLASS_FILE_EXT)), '\\/', '..');
-              $package= substr($qname, 0, strrpos($qname, '.'));
+              $sep= strrpos($qname, '.');
+              $package= substr($qname, 0, $sep);
               if (!isset($packages[$package])) {
                 $packages[$package]= $model->append($p, array($this->pixbuf('package'), $package, NULL));
               }
               
-              $model->append($packages[$package], array($this->pixbuf('class'), $qname, $class));
+              // Determine type. FIXME: Use doclet or something...
+              $type= 'class';
+              $st= new StreamTokenizer($class->getInputStream(), ' ');
+              while ($st->hasMoreTokens()) {
+                $t= $st->nextToken();
+                switch ($t) {
+                  case 'interface': $type= 'interface'; break 2;
+                  case 'extends': switch ($st->nextToken()) {
+                    case 'TestCase': $type= 'test'; break 2;
+                    case 'Enum': $type= 'enum'; break 2;
+                    case 'Command': $type= 'cli'; break 2;
+                  }
+                  break 2;
+                }
+              }
+              
+              $model->append($packages[$package], array($this->pixbuf($type), substr($qname, $sep+ 1), $class));
             }
           }
         }
@@ -88,11 +105,83 @@
       } else if ($value instanceof IOCollection && !$model->iter_has_child($it)) {
         $this->addResources($model, $it, $value);
       } else if ($value instanceof IOElement) {
-        $uri= strtr($value->getURI(), DIRECTORY_SEPARATOR, "/");
-        if (":" == $uri{1} && "/" == $uri{2}) {
-          $uri= "/cygdrive/".$uri{0}.substr($uri, 2);
+        if (!$this->files->containsKey($value)) {
+          // FIXME: Move to own class {{{
+          $src= new GtkSourceLanguagesManager();
+          $lang= $src->get_language_from_mime_type('text/x-xp-source');    // FIXME: Figure out by actual mime
+          var_dump($lang->get_style_scheme()->get_style_names());
+          
+          $buffer= new GtkSourceBuffer();
+          $buffer->set_language($lang);
+          $buffer->set_highlight(TRUE);
+          $buffer->begin_not_undoable_action();  
+          $buffer->set_text(Streams::readAll($value->getInputStream()));
+          $buffer->end_not_undoable_action(); 
+          
+          $source= new GtkSourceView();
+          $source->set_buffer($buffer);
+          $source->set_show_line_numbers(TRUE);
+          $source->set_show_line_markers(TRUE);
+          $source->modify_font(new PangoFontDescription('Courier New 9'));
+
+          // FIXME: Cache this
+          $editor= GladeSource::parse($this->getClass()->getPackage()->getResource('source-editor.glade'));
+
+          $editor->getWidget('container')->handle->add($source);
+
+          $layout= new GtkVBox();   // FIXME: Element is here only for reparent()ing purpose
+          $main= $editor->getWidget('main')->handle;
+          $main->reparent($layout);
+          $layout->show_all();
+          // }}}
+          
+          /*
+          $scintilla= new GtkScintilla();
+          $scintilla->set_styling(-1, 'PHP');
+          $scintilla->insert_text(-1, Streams::readAll($value->getInputStream()));
+
+          // FIXME: Cache this
+          $editor= GladeSource::parse($this->getClass()->getPackage()->getResource('source-editor.glade'));
+
+          $editor->getWidget('container')->handle->add($scintilla);
+
+          $layout= new GtkVBox();   // FIXME: Element is here only for reparent()ing purpose
+          $main= $editor->getWidget('main')->handle;
+          $main->reparent($layout);
+          $layout->show_all();
+          */
+          
+          // [ Label [x] ]
+          $tab= new GtkHBox();
+          $tab->pack_start(GtkImage::new_from_pixbuf($model->get_value($it, 0)));
+          $tab->pack_start(new GtkLabel($model->get_value($it, 1)));
+          $button= new Button();
+          $button->handle->set_image(GtkImage::new_from_pixbuf($this->pixbuf('close')));
+          $button->handle->set_relief(Gtk::RELIEF_NONE);
+          $button->handle->set_size_request(18, 18);
+                    
+          $tab->pack_start($button->handle, FALSE);
+          $tab->show_all();
+          
+          $id= $this->tabs->handle->append_page(
+            $layout, 
+            $tab
+          );
+
+          $button->connect(newinstance(Button::Clicked, array($this, $value), '{
+            public function __construct($main, $value) {
+              $this->main= $main;
+              $this->value= $value;
+            }
+            public function onClicked($source) {
+              $this->main->tabs->handle->remove_page($this->main->files[$this->value]->intValue());
+              $this->main->files->remove($this->value);
+            }
+          }'));       
+
+          $this->files->put($value, new Integer($id));
         }
-        $this->editorCommand('"'.$uri.'"');
+        $this->tabs->setCurrentPage($this->files[$value]->intValue());
       }
     }
     
@@ -110,12 +199,9 @@
 
     public function run($args) {
       $package= $this->getClass()->getPackage();
-      
+      $this->files= new HashTable();
       $p= GladeSource::parse($package->getResource('ide.glade'));
       $w= cast($p->getWidget('main'), 'org.gnome.gtk.Window');
-      $w->showAll();
-      
-      $this->server= new Process('nedit', array('-server', '-svrname '.__CLASS__));
       
       with ($container= $p->getWidget('treeview')); {
         $this->projects= new GtkTreeStore(
@@ -144,6 +230,9 @@
         $completion->set_text_column(1);
         $input->handle->set_completion($completion);
       }
+
+      $this->tabs= $p->getWidget('tabs');
+      $w->showAll();
       
       $this->loadProjects($args[0]);
 
@@ -154,8 +243,6 @@
         }
         public function onDelete($source, $event) {
           Gtk::main_quit();
-          $this->main->editorCommand("-do \'exit()\'");
-          $this->main->server->close();
         }
       }'));
       Gtk::main();
