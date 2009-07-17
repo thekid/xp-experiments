3718ed852923eb5ac2034dae8b34914fc0ac3502
ruben <ruben@svn.xp-framework.net>
2009-07-17 14:07:28 +0000

    - add "PENG"
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11271 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ people/wagner/xp.ide/gedit/xpide/__init__.py
@@ -0,0 +1,36 @@
+
+import gtk
+import os
+import gedit
+
+class XpIdeUi:
+    def __init__(self, plugin, window):
+        self._window= window
+        self._plugin= plugin
+        self._ui= self._window.get_ui_manager().add_ui_from_file(os.path.join(os.path.dirname(__file__), "xpide.ui"))
+        self._ag= gtk.ActionGroup("XpIdePluginActions")
+
+        self._window.get_ui_manager().insert_action_group(self._ag, -1);
+        self._ag.add_action(gtk.Action("XpIde", "XP", None, None))
+        
+        completeAction= gtk.Action("XpIdeComplete", "complete", None, None)
+        self._ag.add_action_with_accel(completeAction, "<Shift><Control>space")
+        completeAction.connect("activate", self._plugin.complete, self._window)
+
+    def shutdown(self):
+        self._window.get_ui_manager().remove_ui(self._ui)
+        self._window.get_ui_manager().remove_action_group(self._ag)
+        self._window.get_ui_manager().ensure_update()
+
+class XpIdePlugin(gedit.Plugin):
+    def activate(self, window):
+        self._ui= XpIdeUi(self, window)
+
+    def deactivate(self, window):
+        self._ui.shutdown()
+
+    def update_ui(self, window):
+        pass
+        
+    def complete(self, action, window):
+        window.get_active_document().insert_at_cursor("PENG")
