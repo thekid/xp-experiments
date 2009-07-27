450bdaf5114705489892c6572b6174ca6a37912b
ruben <ruben@svn.xp-framework.net>
2009-07-27 14:14:29 +0000

    - all dialogs are glade generated
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11296 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/wagner/xp.ide/gedit/xpide/__init__.py
+++ people/wagner/xp.ide/gedit/xpide/__init__.py
@@ -5,6 +5,58 @@ import gedit
 import os
 import dialog
 
+class XpIdePlugin(gedit.Plugin):
+    def openclass(self, action, window):
+        pass
+
+    def complete(self, action, window):
+        tb= window.get_active_document()
+        cursor= tb.get_iter_at_mark(tb.get_insert())
+        completion= subprocess.Popen(
+            [
+                "xpide",
+                "xp.ide.completion.Runner",
+                "Gedit",
+                "-p", str(cursor.get_offset()),
+                "-l", str(cursor.get_line()),
+                "-c", str(cursor.get_line_offset()),
+            ],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE
+        )
+        completion.stdin.write(tb.get_text(tb.get_start_iter(), tb.get_end_iter()))
+        completion.stdin.close()
+        
+        rep_pos= int(completion.stdout.next())
+        rep_len= int(completion.stdout.next())
+        sug_cnt= int(completion.stdout.next())
+        if (sug_cnt == 0):
+          hint= dialog.TextCalltip(master=window, title="")
+          hint.setText("no object found")
+          hint.run()
+          return
+        if (sug_cnt == 1):
+            suggestion= completion.stdout.next().strip()
+        else:
+            sug_dialog= dialog.TextSelectCalltip(master= window, title="")
+            for sug in completion.stdout: sug_dialog.addOption(sug.strip())
+            suggestion= sug_dialog.run()
+            if (suggestion is None): return
+        tb.delete(
+            tb.get_iter_at_offset(rep_pos),
+            tb.get_iter_at_offset(rep_pos + rep_len)
+        )
+        tb.insert(tb.get_iter_at_offset(rep_pos), suggestion)
+
+    def activate(self, window):
+        self._ui= XpIdeUi(self, window)
+
+    def deactivate(self, window):
+        self._ui.shutdown()
+
+    def update_ui(self, window):
+        pass
+        
 class XpIdeUi:
     def __init__(self, plugin, window):
         self._window= window
@@ -23,53 +75,4 @@ class XpIdeUi:
         self._window.get_ui_manager().remove_ui(self._ui)
         self._window.get_ui_manager().remove_action_group(self._ag)
         self._window.get_ui_manager().ensure_update()
-
-class XpIdePlugin(gedit.Plugin):
-    def activate(self, window):
-        self._ui= XpIdeUi(self, window)
-
-    def deactivate(self, window):
-        self._ui.shutdown()
-
-    def update_ui(self, window):
-        pass
-        
-    def complete(self, action, window):
-        tb= window.get_active_document()
-        cursor= tb.get_iter_at_mark(tb.get_insert())
-        completion= subprocess.Popen(
-            [
-                "xpide",
-                "xp.ide.completion.Runner",
-                "xp.ide.completion.Gedit",
-                "-p", str(cursor.get_offset()),
-                "-l", str(cursor.get_line()),
-                "-c", str(cursor.get_line_offset()),
-            ],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE
-        )
-        completion.stdin.write(tb.get_text(tb.get_start_iter(), tb.get_end_iter()))
-        completion.stdin.close()
-        
-        rep_pos= int(completion.stdout.next())
-        rep_len= int(completion.stdout.next())
-        sug_cnt= int(completion.stdout.next())
-        if (sug_cnt == 0):
-          hint= dialog.TextCalltip(master=window, title="")
-          hint.setText("blalvla")
-          hint.run()
-          return
-        if (sug_cnt == 1):
-            suggestion= completion.stdout.next().strip()
-        else:
-            sug_dialog= dialog.TextSelectCalltip(master= window, title="")
-            for sug in completion.stdout: sug_dialog.addOption(sug.strip())
-            suggestion= sug_dialog.run()
-            if (suggestion is None): return
-        tb.delete(
-            tb.get_iter_at_offset(rep_pos),
-            tb.get_iter_at_offset(rep_pos + rep_len)
-        )
-        tb.insert(tb.get_iter_at_offset(rep_pos), suggestion)
 
--- people/wagner/xp.ide/gedit/xpide/dialog.py
+++ people/wagner/xp.ide/gedit/xpide/dialog.py
@@ -3,80 +3,59 @@ import gobject
 import gtk
 import os
 
-class Calltip(gtk.Dialog):
-    def __init__(self, master=None, title=None):
-        gtk.Dialog.__init__(self)
-        if master: self.set_transient_for(master)
-        if title:  self.set_title(title)
-        self.set_modal(True)
-        self.set_destroy_with_parent(True)
-        self.set_has_separator(False)
-        self.set_resizable(False)
-        self.set_decorated(False)
-    
+class Calltip():
+    def __init__(self, dialog, master=None, title=None):
+        self._builder= gtk.Builder()
+        self._builder.add_from_file(os.path.join(os.path.dirname(__file__), "dialog", dialog + ".ui"))
+        self._dialog= self._builder.get_object(dialog)
+        if master: self._dialog.set_transient_for(master)
+        if title:  self._dialog.set_title(title)
+
     def run(self):
-        ret= gtk.Dialog.run(self)
-        self.hide()
+        self._dialog.show()
+        ret= self._dialog.run()
+        self._dialog.hide()
         return ret
 
 class TextSelectCalltip(Calltip):
     def __init__(self, master=None, title=None):
-        Calltip.__init__(self, master, title)
+        Calltip.__init__(self, "TextSelectCalltip", master, title)
+
         list_col= gtk.TreeViewColumn(None, gtk.CellRendererText(), text= 0)
         list_col.set_sort_order(gtk.SORT_ASCENDING)
-        self.list= gtk.ListStore(gobject.TYPE_STRING)
-        self.view= gtk.TreeView(self.list)
-        self.view.set_headers_visible(False)
-        self.view.append_column(list_col)
-        self.view.connect("key-press-event", self.keypressEH)
-        self.view.connect("button-press-event", self.buttonpressEH)
-        self.view.show()
-        self.vbox.pack_end(self.view)
+        self._list= gtk.ListStore(gobject.TYPE_STRING)
+        self._view= self._builder.get_object("suggestions")
+        self._view.set_model(self._list)
+        self._view.append_column(list_col)
+        self._view.connect("key-press-event", self.keypressEH)
+        self._view.connect("button-press-event", self.buttonpressEH)
     
     def addOption(self, text):
-        self.list.set(self.list.append(), 0, text)
+        self._list.set(self._list.append(), 0, text)
         
     def run(self):
         ret= Calltip.run(self)
         if (ret != gtk.RESPONSE_OK): return None
-        ret_iter= self.view.get_selection().get_selected()[1]
+        ret_iter= self._view.get_selection().get_selected()[1]
         if (ret_iter is None): return None
-        return self.list.get(ret_iter, 0)[0]
+        return self._list.get(ret_iter, 0)[0]
 
     def keypressEH(self, widget, event):
         if (gtk.gdk.keyval_name(event.keyval) in ["Return", "space"]):
-            self.response(gtk.RESPONSE_OK);
+            self._dialog.response(gtk.RESPONSE_OK);
             return True
         return False
 
     def buttonpressEH(self, widget, event):
         if (gtk.gdk._2BUTTON_PRESS == event.type):
-            self.response(gtk.RESPONSE_OK);
+            self._dialog.response(gtk.RESPONSE_OK);
             return True
         return False
 
-class TextCalltip():
+class TextCalltip(Calltip):
     def __init__(self, master=None, title=None):
-        self._builder= gtk.Builder()
-        self._builder.add_from_file(os.path.join(os.path.dirname(__file__), "dialog", "TextCalltip.ui"))
-        self._dialog= self._builder.get_object("TextCalltip")
-        if master: self._dialog.set_transient_for(master)
-        if title:  self._dialog.set_title(title)
+        Calltip.__init__(self, "TextCalltip", master, title)
         self._hint= self._builder.get_object("hint")
 
     def setText(self, text):
         self._hint.set_text(text)
-
-    def run(self):
-        self._dialog.show()
-        self._dialog.run()
-        self._dialog.hide()
-
-    def keypressEH(self, widget, event):
-        self.response(gtk.RESPONSE_OK);
-        return True
-
-    def buttonpressEH(self, widget, event):
-        self.response(gtk.RESPONSE_OK);
-        return True
-
