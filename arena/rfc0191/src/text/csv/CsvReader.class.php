71084ae6bc016022ac3fb39283333cefd95ee227
Timm Friebe <thekid@thekid.de>
2009-09-13 13:58:06 +0000

    - Add getHeader() method - Add line number to exception messages - QA: Add @throws to readValues` apidoc
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11444 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/rfc0191/src/text/csv/CsvReader.class.php
+++ arena/rfc0191/src/text/csv/CsvReader.class.php
@@ -17,6 +17,7 @@
     protected $reader= NULL;
     protected $separator= ';';
     protected $quote= '"';
+    protected $line= 0;
    
     /**
      * Creates a new CSV reader reading data from a given TextReader
@@ -26,15 +27,30 @@
     public function  __construct(TextReader $reader) {
       $this->reader= $reader;
     }
+
+    /**
+     * Get header line
+     *
+     * @return  string[]
+     * @throws  lang.IllegalStateException if reading has already started
+     */
+    public function getHeaders() {
+      if ($this->line > 0) {
+        throw new IllegalStateException('Cannot read headers - already started reading data');
+      }
+      return $this->readValues();
+    }
     
     /**
      * Reads values
      *
      * @return  string[]
+     * @throws  lang.FormatException if a formatting error is detected
      */
     protected function readValues() {
       if (NULL === ($line= $this->reader->readLine())) return NULL;
-      
+
+      $this->line++;
       $values= array();
       $o= 0; $l= strlen($line);
       while (FALSE !== ($p= strcspn($line, $this->separator, $o))) {
@@ -42,11 +58,11 @@
           $value= '';
         } else if ($this->quote === $line{$o}) {
           if ($o === $p- 1) {
-            throw new FormatException('Unterminated quoted value in ['.$line.']');
+            throw new FormatException('Unterminated quoted value in ['.$line.'] (line '.$this->line.')');
           } else if ($this->quote !== $line{$p- 1}) {
             $p= strcspn($line, $this->quote, $o+ 1)+ 2;   // leading and trailing quote
             if ($o+ $p > $l) {
-              throw new FormatException('Unterminated quoted value in ['.$line.']');
+              throw new FormatException('Unterminated quoted value in ['.$line.'] (line '.$this->line.')');
             }
           }
           $value= str_replace($this->quote.$this->quote, $this->quote, substr($line, $o+ 1, $p- 2));
