379308af1b9cd096ec718c57db39489fec54fdb6
Timm Friebe <thekid@thekid.de>
2009-10-10 21:03:44 +0000

    - Fix "Argument 1 passed to Scope::resolveType() must be an   instance of TypeName, string given"
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11574 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/lang/xp/compiler/types/Scope.class.php
+++ arena/lang/xp/compiler/types/Scope.class.php
@@ -252,10 +252,10 @@
             $tree= $this->task->newSubTask($qualified)->run($this);
             switch ($decl= $tree->declaration) {
               case $decl instanceof ClassNode: 
-                $t= new TypeDeclaration($tree, $this->resolveType($decl->parent ? $decl->parent->name : new TypeName('lang.Object')));
+                $t= new TypeDeclaration($tree, $this->resolveType($decl->parent ? $decl->parent : new TypeName('lang.Object')));
                 break;
               case $decl instanceof EnumNode:
-                $t= new TypeDeclaration($tree, $this->resolveType($decl->parent ? $decl->parent->name : new TypeName('lang.Enum')));
+                $t= new TypeDeclaration($tree, $this->resolveType($decl->parent ? $decl->parent : new TypeName('lang.Enum')));
                 break;
               case $decl instanceof InterfaceNode:
                 $t= new TypeDeclaration($tree, NULL);
