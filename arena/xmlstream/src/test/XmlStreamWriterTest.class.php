1d7ab53444646ab6ecf26f91afdb13f9a2ce1499
Timm Friebe <thekid@thekid.de>
2009-11-20 22:56:22 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11680 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ arena/xmlstream/src/test/XmlStreamWriterTest.class.php
@@ -0,0 +1,372 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'xml.streams.XmlStreamWriter',
+    'io.streams.MemoryOutputStream'
+  );
+
+  /**
+   * TestCase
+   *
+   * @see      xp://xml.streams.XmlStreamWriter
+   */
+  class XmlStreamWriterTest extends TestCase {
+  
+    /**
+     * Sets up test case
+     *
+     */
+    public function setUp() {
+      $this->stream= new MemoryOutputStream();
+      $this->writer= new XmlStreamWriter($this->stream);
+    }
+
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function initiallyEmpty() {
+      $this->assertEquals('', $this->stream->getBytes());
+    }
+
+    /**
+     * Test startDocument() method
+     *
+     */
+    #[@test]
+    public function startDocument() {
+      $this->writer->startDocument();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test startDocument() method
+     *
+     */
+    #[@test]
+    public function startUtf8Document() {
+      $this->writer->startDocument('utf-8');
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="utf-8"?>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test startDocument() method
+     *
+     */
+    #[@test, @expect('lang.IllegalStateException')]
+    public function startDocumentTwice() {
+      $this->writer->startDocument();
+      $this->writer->startDocument();
+    }
+
+    /**
+     * Test closeDocument() method
+     *
+     */
+    #[@test]
+    public function closeDocument() {
+      $this->writer->startDocument();
+      $this->writer->closeDocument();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test closeDocument() method
+     *
+     */
+    #[@test, @expect('lang.IllegalStateException')]
+    public function closeDocumentBeforeStart() {
+      $this->writer->closeDocument();
+    }
+
+    /**
+     * Test closeDocument() method
+     *
+     */
+    #[@test]
+    public function closeDocumentClosesOpenTags() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->closeDocument();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root></root>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test startNode() method
+     *
+     */
+    #[@test]
+    public function writeRootNode() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test startNode() method
+     *
+     */
+    #[@test]
+    public function withAttribute() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root', array('version' => '1.0'));
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root version="1.0">', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test startNode() method
+     *
+     */
+    #[@test]
+    public function withAttributes() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root', array('version' => '1.0', 'id' => '-//XP/5.7.6'));
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root version="1.0" id="-//XP/5.7.6">', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test startNode() method
+     *
+     */
+    #[@test]
+    public function withAttributeContainingSpecialChars() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root', array('name' => '<Hello&>'));
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root name="&lt;Hello&amp;&gt;">', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test startNode() method
+     *
+     */
+    #[@test]
+    public function withAttributeContainingQuotes() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root', array('name' => '"Hello"'));
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root name="&quot;Hello&quot;">', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test writeEmptyNode() method
+     *
+     */
+    #[@test]
+    public function emptyRootNode() {
+      $this->writer->startDocument();
+      $this->writer->writeEmptyNode('root');
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root/>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test startNode() and closeNode() methods
+     *
+     */
+    #[@test]
+    public function completeRootNode() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->closeNode();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root></root>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test startNode() and closeNode() methods
+     *
+     */
+    #[@test]
+    public function childNode() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->startNode('document');
+      $this->writer->closeNode();
+      $this->writer->closeNode();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root><document></document></root>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test writeCharacters() method
+     *
+     */
+    #[@test]
+    public function writeCharacters() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->writeCharacters('Hello');
+      $this->writer->closeNode();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root>Hello</root>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test writeCharacters() method escapes special chars
+     *
+     */
+    #[@test]
+    public function writeCharactersEscapesSpecial() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->writeCharacters('<nedit&>');
+      $this->writer->closeNode();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root>&lt;nedit&amp;&gt;</root>', 
+        $this->stream->getBytes()
+      );
+    }
+ 
+    /**
+     * Test writeCharacters() method does not escape double quotes
+     *
+     */
+    #[@test]
+    public function writeCharactersDoesNotEscapeDoubleQuotes() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->writeCharacters('"Hello"');
+      $this->writer->closeNode();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root>"Hello"</root>', 
+        $this->stream->getBytes()
+      );
+    }
+ 
+    /**
+     * Test writeCharacters() method does not escape single quotes
+     *
+     */
+    #[@test]
+    public function writeCharactersDoesNotEscapeSingleQuotes() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->writeCharacters('\'Hello\'');
+      $this->writer->closeNode();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root>\'Hello\'</root>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test writeCData() method
+     *
+     */
+    #[@test]
+    public function writeCData() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->writeCData('Hello');
+      $this->writer->closeNode();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root><![CDATA[Hello]]></root>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test writeCData() method
+     *
+     */
+    #[@test]
+    public function writeCDataWithCDataEndInside() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->writeCData('Hello]]>');
+      $this->writer->closeNode();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root><![CDATA[Hello]]]]><![CDATA[>]]></root>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test writeComment() method
+     *
+     */
+    #[@test]
+    public function writeComment() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->writeComment('Hello');
+      $this->writer->closeNode();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root><!-- Hello --></root>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test writeProcessingInstruction() method
+     *
+     */
+    #[@test]
+    public function writeProcessingInstruction() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->writeProcessingInstruction('php', 'echo "Hello";');
+      $this->writer->closeNode();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root><?php echo "Hello"; ?></root>', 
+        $this->stream->getBytes()
+      );
+    }
+
+    /**
+     * Test writeEntityRef() method
+     *
+     */
+    #[@test]
+    public function writeEntityRef() {
+      $this->writer->startDocument();
+      $this->writer->startNode('root');
+      $this->writer->writeEntityRef('data');
+      $this->writer->closeNode();
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?><root>&data;</root>', 
+        $this->stream->getBytes()
+      );
+    }
+  }
+?>
---
+++ arena/xmlstream/src/xml/streams/XmlStreamWriter.class.php
@@ -0,0 +1,135 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('io.streams.OutputStream');
+
+  /**
+   * Writes XML
+   *
+   * @test     xp://test.XmlStreamWriterTest
+   */
+  class XmlStreamWriter extends Object {
+    protected $stream= NULL;
+    protected $opened= NULL;
+    
+    /**
+     * Creates a new XML stream writer
+     *
+     * @param   io.streams.OutputStream stream
+     */
+    public function __construct(OutputStream $stream) {
+      $this->stream= $stream;
+    }
+    
+    /**
+     * Starts document
+     *
+     * @param   string encoding default "iso-8859-1"
+     * @throws  lang.IllegalStateException in case document has already been started
+     */
+    public function startDocument($encoding= 'iso-8859-1') {
+      if (NULL !== $this->opened) {
+        throw new IllegalStateException('Document already started');
+      }
+      $this->stream->write('<?xml version="1.0" encoding="'.$encoding.'"?>');
+      $this->opened= array();
+    }
+
+    /**
+     * Ends document. Closes any open tags
+     *
+     * @throws  lang.IllegalStateException in case document has not yet been started
+     */
+    public function closeDocument() {
+      if (NULL === $this->opened) {
+        throw new IllegalStateException('Document already started');
+      }
+      while ($name= array_pop($this->opened)) {
+        $this->stream->write('</'.$name.'>');
+      }
+    }
+
+    /**
+     * Starts a node
+     *
+     * @param   string name
+     * @param   array<string, string> attributes
+     */
+    public function startNode($name, array $attributes= array()) {
+      $this->stream->write('<'.$name);
+      foreach ($attributes as $attribute => $value)  {
+        $this->stream->write(' '.$attribute.'="'.htmlspecialchars($value, ENT_QUOTES).'"');
+      }
+      $this->stream->write('>');
+      $this->opened[]= $name;
+    }
+    
+    /**
+     * Closes a node
+     *
+     * @param   string name
+     */
+    public function closeNode() {
+      $this->stream->write('</'.array_pop($this->opened).'>');
+    }
+
+    /**
+     * Writes an empty node
+     *
+     * @param   string name
+     */
+    public function writeEmptyNode($name) {
+      $this->stream->write('<'.$name.'/>');
+    }
+
+    /**
+     * Write character data
+     *
+     * @param   string text
+     */
+    public function writeCharacters($text) {
+      $this->stream->write(htmlspecialchars($text, ENT_NOQUOTES));
+    }
+
+    /**
+     * Write CDATA section
+     *
+     * @param   string text
+     */
+    public function writeCData($text) {
+      $this->stream->write('<![CDATA['.str_replace(']]>', ']]]]><![CDATA[>', $text).']]>');
+    }
+
+    /**
+     * Write comment
+     *
+     * @param   string text
+     */
+    public function writeComment($text) {
+      $this->stream->write('<!-- '.$text.' -->');
+    }
+
+    /**
+     * Write processing instruction
+     *
+     * @param   string target
+     * @param   string text
+     */
+    public function writeProcessingInstruction($target, $text) {
+      $this->stream->write('<?'.$target.' '.$text.' ?>');
+    }
+
+    /**
+     * Write processing instruction
+     *
+     * @param   string target
+     * @param   string text
+     */
+    public function writeEntityRef($name) {
+      $this->stream->write('&'.$name.';');
+    }
+  }
+?>
