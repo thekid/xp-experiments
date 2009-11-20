0a118ea87954e0fd1d8caf1206d72bf6fc559964
Timm Friebe <thekid@thekid.de>
2009-11-20 20:55:09 +0000

    - Fix failing tests by changing string representation creation
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11677 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/logging/src/util/log/LoggingEvent.class.php
+++ arena/logging/src/util/log/LoggingEvent.class.php
@@ -32,7 +32,19 @@
       $this->timestamp= $timestamp;
       $this->processId= $processId;
       $this->level= $level;
-      $this->message= implode(' ', array_map(array('xp', 'stringOf'), $arguments));
+      $this->message= implode(' ', array_map(array($this, 'stringOf'), $arguments));
+    }
+    
+    /**
+     * Creates a string representation of the given argument. For any 
+     * string given, the result is the string itself, for any other type,
+     * the result is the xp::stringOf() output.
+     *
+     * @param   var arg
+     * @return  string
+     */
+    protected function stringOf($arg) {
+      return is_string($arg) ? $arg : xp::stringOf($arg);
     }
         
     /**
