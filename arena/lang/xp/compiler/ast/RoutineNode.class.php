4b04de2ddcc093b469228aae716a2a3cdb680f43
Timm Friebe <thekid@thekid.de>
2009-12-31 19:00:34 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11809 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ arena/lang/xp/compiler/ast/RoutineNode.class.php
@@ -0,0 +1,19 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('xp.compiler.ast.Node');
+
+  /**
+   * Abstract base class for all routines
+   *
+   * @see   xp://xp.compiler.ast.MethodNode
+   * @see   xp://xp.compiler.ast.ConstructorNode
+   * @see   xp://xp.compiler.ast.OperatorNode
+   */
+  abstract class RoutineNode extends xp·compiler·ast·Node {
+    
+  }
+?>
