ff0ac88bac8ae436fbce3046bc687d9016d4340a
Timm Friebe <thekid@thekid.de>
2010-01-01 22:53:49 +0000

    - Initial implementation
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11817 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/zip/io/zip/ZipArchiveReader.class.php
+++ arena/zip/io/zip/ZipArchiveReader.class.php
@@ -4,34 +4,47 @@
  * $Id$ 
  */
 
+  uses('io.streams.InputStream', 'io.zip.Compression', 'util.Date');
+
   /**
-   * (Insert class' description here)
+   * Read from a zip file
    *
-   * @ext      extension
-   * @see      reference
-   * @purpose  purpose
+   * @see      xp://io.zip.ZipArchive#open
+   * @purpose  Write to a zip archive
    */
   class ZipArchiveReader extends Object {
+    protected $stream= NULL;
+
+    const EOCD = "\x50\x4b\x05\x06";
+    const FHDR = "\x50\x4b\x03\x04";
+    const DHDR = "\x50\x4b\x01\x02";
 
     /**
-     * Returns whether this zip file contains an entry by a given name
+     * Creation constructor
      *
-     * @param   string name
-     * @return  bool
+     * @param   io.streams.InputStream stream
      */
-    public function hasEntry($name) {
-    
+    public function __construct(InputStream $stream) {
+      $this->stream= $stream;
     }
-
-    /**
-     * Returns a zip entry for a given name
-     *
-     * @param   string name
-     * @return  io.zip.ZipEntry
-     * @throws  lang.ElementNotFoundException
-     */
-    public function getEntry($name) {
     
+    /**
+     * Creates a date from DOS date and time
+     *
+     * @see     http://www.vsft.com/hal/dostime.htm
+     * @param   int date
+     * @param   int time
+     * @return  util.Date
+     */
+    protected function dateFromDosDateTime($date, $time) {
+      return Date::create(
+        (($date >> 9) & 0x7F) + 1980,
+        (($date >> 5) & 0x0F),
+        $date & 0x1F,
+        ($time >> 11) & 0x1F,
+        ($time >> 5) & 0x3F,
+        ($time << 1) & 0x1E
+      );
     }
 
     /**
@@ -40,8 +53,44 @@
      * @return  io.zip.ZipEntry[]
      */
     public function entries() {
-    
+      $done= FALSE;
+      $r= array();
+      do {
+        $header= $this->stream->read(4);
+        switch ($header) {
+          case self::FHDR: {      // Entry
+            $info= unpack(
+              'vversion/vflags/vcompression/vtime/vdate/Vcrc/Vcompressed/Vuncompressed/vnamelen/vextralen', 
+              $this->stream->read(26)
+            );
+            $name= $this->stream->read($info['namelen']);
+            $extra= $this->stream->read($info['extralen']);
+            $this->stream->read($info['compressed']);
+            
+            Console::writeLinef(
+              '%s: %.2f kB / %s @ %s',
+              $name, 
+              $info['uncompressed'] / 1024,
+              Compression::getInstance($info['compression'])->name(),
+              $this->dateFromDosDateTime($info['date'], $info['time'])->toString('Y-m-d H:i:s')
+            );
+            $r[]= new ZipFileEntry($name, $this->dateFromDosDateTime($info['date'], $info['time']));
+            break;
+          }
+          case self::DHDR: {      // Zip directory
+            $done= TRUE;
+            break;
+          }
+          case self::EOCD: {      // End of central directory
+            $done= TRUE;
+            break;
+          }
+          default: {
+            throw new FormatException('Unknown byte sequence '.addcslashes($header, "\0..\17"));
+          }
+        }
+      } while (!$done);
+      return $r;
     }
-    
   }
 ?>
