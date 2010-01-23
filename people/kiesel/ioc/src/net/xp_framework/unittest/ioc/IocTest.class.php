362ff5edad927000ff0069eb856d08415948cbe9
Timm Friebe <thekid@thekid.de>
2010-01-23 12:47:06 +0000

    - Add method injection test
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11883 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/kiesel/ioc/src/net/xp_framework/unittest/ioc/IocTest.class.php
+++ people/kiesel/ioc/src/net/xp_framework/unittest/ioc/IocTest.class.php
@@ -16,14 +16,23 @@
    *
    */
   class IocTest extends TestCase {
+    protected $fixtureClass= NULL;
+  
+    /**
+     * Populates fixtureClass member with a unique name
+     *
+     */
+    public function setUp() {
+      $this->fixtureClass= 'Ioc'.$this->name;
+    }
     
     /**
      * Tests constructor injection
      *
      */
     #[@test]
-    public function injectConstructor() {
-      $class= ClassLoader::defineClass('IocSimpleClass', 'lang.Object', array(), '{
+    public function constructorInjection() {
+      $class= ClassLoader::defineClass($this->fixtureClass, 'lang.Object', array(), '{
         protected $injectee = NULL;
         
         #[@inject]
@@ -40,8 +49,36 @@
         }
       }'));
       
-      $instance= $injector->get('IocSimpleClass');
-      $this->assertInstanceOf('IocSimpleClass', $instance);
+      $instance= $injector->get($this->fixtureClass);
+      $this->assertInstanceOf($this->fixtureClass, $instance);
+      $this->assertInstanceOf('InjecteeImpl', $instance->getInjectee());
+    }
+
+    /**
+     * Tests method injection
+     *
+     */
+    #[@test]
+    public function methodInjection() {
+      $class= ClassLoader::defineClass($this->fixtureClass, 'lang.Object', array(), '{
+        protected $injectee = NULL;
+        
+        #[@inject]
+        public function setInjectee(Injectee $i) {
+          $this->injectee= $i;
+        }
+        
+        public function getInjectee() { return $this->injectee; }
+      }');
+      
+      $injector= IoC::getInjectorFor(newinstance('ioc.AbstractModule', array(), '{
+        public function configure() {
+          $this->bind(XPClass::forName("Injectee"))->to(XPClass::forName("InjecteeImpl"));
+        }
+      }'));
+      
+      $instance= $injector->get($this->fixtureClass);
+      $this->assertInstanceOf($this->fixtureClass, $instance);
       $this->assertInstanceOf('InjecteeImpl', $instance->getInjectee());
     }
   }
