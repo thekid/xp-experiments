3392a395b4d46bb4a127633ec16aa7b935ac9b2d
Timm Friebe <thekid@thekid.de>
2010-01-31 10:58:51 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11923 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ arena/lang/tests/checks/ConstantsAreDiscouragedTest.class.php
@@ -0,0 +1,41 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'xp.compiler.checks.ConstantsAreDiscouraged',
+    'xp.compiler.ast.ConstantNode'
+  );
+
+  /**
+   * TestCase
+   *
+   * @see      xp://xp.compiler.checks.ConstantsAreDiscouraged
+   */
+  class ConstantsAreDiscouragedTest extends TestCase {
+    protected $fixture= NULL;
+  
+    /**
+     * Sets up test case
+     *
+     */
+    public function setUp() {
+      $this->fixture= new ConstantsAreDiscouraged();
+    }
+    
+    /**
+     * Test constants 
+     *
+     */
+    #[@test]
+    public function constantsAreDiscouraged() {
+      $this->assertEquals(
+        array('T203', 'Global constants (DIRECTORY_SEPARATOR) are discouraged'), 
+        $this->fixture->verify(new ConstantNode(array('value' => 'DIRECTORY_SEPARATOR')))
+      );
+    }
+  }
+?>
