562fe315f79049a260448f3cdead8c7000b94f69
Timm Friebe <thekid@thekid.de>
2010-02-06 21:06:49 +0000

    - Cast array and map types with (array) in PHP
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12024 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/lang/xp/compiler/emit/source/Emitter.class.php
+++ arena/lang/xp/compiler/emit/source/Emitter.class.php
@@ -262,8 +262,11 @@
         // Missing intentionally: object and unset casts
       );
 
-      if (isset($primitives[$cast->type->name])) {
+      if ($cast->type->isPrimitive()) {
         $op->append($primitives[$cast->type->name]);
+        $this->emitOne($op, $cast->expression);
+      } else if ($cast->type->isArray() || $cast->type->isMap()) {
+        $op->append('(array)');
         $this->emitOne($op, $cast->expression);
       } else {
         $op->append('cast(');
