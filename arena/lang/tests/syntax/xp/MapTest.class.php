a59b7d730d3881d26361bda3781831af8243d7a2
Timm Friebe <thekid@thekid.de>
2010-02-06 18:38:07 +0000

    - Allow dangling commas in map literals
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12014 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/lang/tests/syntax/xp/MapTest.class.php
+++ arena/lang/tests/syntax/xp/MapTest.class.php
@@ -54,5 +54,33 @@
         $this->parse('[ 1 : "one", 2 : "two", 3 : "three" ];')
       );
     }
+
+    /**
+     * Test a non-empty untyped map
+     *
+     */
+    #[@test]
+    public function untypedMapWithDanglingComma() {
+      $this->assertEquals(
+        array(new MapNode(array(
+          'elements'      => array(
+            array(
+              new IntegerNode('1'),
+              new StringNode('one'),
+            ),
+            array(
+              new IntegerNode('2'),
+              new StringNode('two'),
+            ),
+            array(
+              new IntegerNode('3'),
+              new StringNode('three'),
+            ),
+          ),
+          'type'          => NULL,
+        ))), 
+        $this->parse('[ 1 : "one", 2 : "two", 3 : "three", ];')
+      );
+    }
   }
 ?>
