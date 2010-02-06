ef8e497b5cb9841ed70da74b5271c3a7d3678587
Timm Friebe <thekid@thekid.de>
2010-02-06 11:17:43 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@11956 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ arena/lang/tests/convert/ArrayRewritingTest.class.php
@@ -0,0 +1,55 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('tests.convert.AbstractConversionTest');
+
+  /**
+   * Tests array syntax is rewritten
+   *
+   * @see      xp://tests.convert.AbstractConversionTest
+   */
+  class ArrayRewritingTest extends AbstractConversionTest {
+
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function intArray() {
+      $this->assertConversion(
+        '$a= [1, 2, 3];',
+        '$a= array(1, 2, 3);',
+        SourceConverter::ST_FUNC_BODY
+      );
+    }
+ 
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function stringArray() {
+      $this->assertConversion(
+        '$a= ["Hello", "World"];',
+        '$a= array("Hello", "World");',
+        SourceConverter::ST_FUNC_BODY
+      );
+    }
+
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function emptyArray() {
+      $this->assertConversion(
+        '$a= [];',
+        '$a= array();',
+        SourceConverter::ST_FUNC_BODY
+      );
+    }
+  }
+?>
