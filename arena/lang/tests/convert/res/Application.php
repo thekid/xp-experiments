d80599621f69546e96801cab43d04d96d79e15a3
Timm Friebe <thekid@thekid.de>
2010-02-13 14:31:45 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12059 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ arena/lang/tests/convert/res/Application.php
@@ -0,0 +1,23 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Application
+   *
+   */
+  class Application extends Object {
+    
+    /**
+     * Entry point
+     *
+     * @param   string[] args
+     */
+    public static function main($args) {
+      $begin= new Date();
+      $end= Date::now();
+    }
+  }
+?>
