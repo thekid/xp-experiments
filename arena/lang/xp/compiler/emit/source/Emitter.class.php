f21b0ddf49ff8caddcd62f11df8b7e2998b1c15b
Timm Friebe <thekid@thekid.de>
2010-02-26 19:40:34 +0000

    - QA: Update list of tests
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12123 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/lang/xp/compiler/emit/source/Emitter.class.php
+++ arena/lang/xp/compiler/emit/source/Emitter.class.php
@@ -28,22 +28,32 @@
   /**
    * Emits sourcecode using PHP sourcecode
    *
+   * @test     xp://tests.execution.source.AnnotationTest
    * @test     xp://tests.execution.source.ArrayTest
+   * @test     xp://tests.execution.source.AssignmentTest
+   * @test     xp://tests.execution.source.CastingTest
    * @test     xp://tests.execution.source.CatchTest
+   * @test     xp://tests.execution.source.ChainingTest
    * @test     xp://tests.execution.source.ClassDeclarationTest
    * @test     xp://tests.execution.source.ComparisonTest
+   * @test     xp://tests.execution.source.ConcatTest
+   * @test     xp://tests.execution.source.DefaultArgsTest
    * @test     xp://tests.execution.source.EnumDeclarationTest
-   * @test     xp://tests.execution.source.ExecutionTest
    * @test     xp://tests.execution.source.ExtensionMethodsTest
    * @test     xp://tests.execution.source.FinallyTest
-   * @test     xp://tests.execution.source.InstanceCreationTestI
+   * @test     xp://tests.execution.source.InstanceCreationTest
    * @test     xp://tests.execution.source.InterfaceDeclarationTest
+   * @test     xp://tests.execution.source.LambdaTest
    * @test     xp://tests.execution.source.LoopExecutionTest
    * @test     xp://tests.execution.source.MathTest
    * @test     xp://tests.execution.source.MultiCatchTest
+   * @test     xp://tests.execution.source.OperatorTest
    * @test     xp://tests.execution.source.PropertiesTest
+   * @test     xp://tests.execution.source.StaticImportTest
+   * @test     xp://tests.execution.source.TernaryOperatorTest
+   * @test     xp://tests.execution.source.VarArgsTest
    * @test     xp://tests.execution.source.VariablesTest
-   * @test     xp://tests.execution.source.AssignmentTest
+   * @test     xp://tests.execution.source.WithTest
    * @see      xp://xp.compiler.ast.Node
    */
   class xp·compiler·emit·source·Emitter extends Emitter {
