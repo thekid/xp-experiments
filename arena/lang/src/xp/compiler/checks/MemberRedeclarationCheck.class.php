f124398b60be127164159dfa402da56c367e0e87
Timm Friebe <thekid@thekid.de>
2010-03-06 11:32:37 +0000

    - Use getName() instead of directly accessing name member
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12150 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/lang/src/xp/compiler/checks/MemberRedeclarationCheck.class.php
+++ arena/lang/src/xp/compiler/checks/MemberRedeclarationCheck.class.php
@@ -34,7 +34,7 @@
       $index= array();
       $qname= $type->name->compoundName();
       foreach ($type->body as $member) {
-        $key= $member->getClassName().'<'.$member->name.'>';
+        $key= $member->getClassName().'<'.$member->getName().'>';
         if (isset($index[$key])) {
           return array('C409', 'Cannot redeclare '.$qname.'::'.$key);
         }
