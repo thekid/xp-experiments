2582aa106bbfa68499bf2406b58b592f53d93050
Timm Friebe <thekid@thekid.de>
2010-06-27 12:56:11 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12317 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ arena/lang/src/net/xp_lang/tests/syntax/xp/UnsupportedFeaturesTest.class.php
@@ -0,0 +1,189 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('net.xp_lang.tests.syntax.xp.ParserTestCase');
+
+  /**
+   * TestCase ensuring the following is not supported in XP language:
+   * 
+   * <ul>
+   *   <li>Variable variables ($$i) and dynamic variables (${$i})</li>
+   *   <li>Goto</li>
+   * </ul>
+   */
+  class UnsupportedFeaturesTest extends ParserTestCase {
+  
+    /**
+     * Test variable variables are not supported
+     *
+     * @see   php://language.variables.variable
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function variableVariables() {
+      $this->parse('$$i= 0;');
+    }
+
+    /**
+     * Test dynamic variables are not supported
+     *
+     * @see   php://language.variables.variable
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function dynamicVariables() {
+      $this->parse('${$i}= 0;');
+    }
+
+    /**
+     * Test goto is not supported
+     *
+     * @see   php://goto
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function gotoStatement() {
+      $this->parse('goto error;');
+    }
+
+    /**
+     * Test functions are not supported
+     *
+     * @see   php://language.functions
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function functions() {
+      $this->parse('function a() { }');
+    }
+
+    /**
+     * Test references are not supported
+     *
+     * @see   php://language.references
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function references() {
+      $this->parse('$a= &$b;');
+    }
+
+    /**
+     * Test "elseif" keyword is not supported
+     *
+     * @see   php://elseif
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function elseifKeyword() {
+      $this->parse('if ($a) { $b++; } elseif ($c) { $d++; }');
+    }
+
+    /**
+     * Test "include" keyword is not supported without braces
+     *
+     * @see   php://include
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function includeKeywordWithoutBraces() {
+      $this->parse('include "functions.inc";');
+    }
+
+    /**
+     * Test "require" keyword is not supported without braces
+     *
+     * @see   php://require
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function requireKeywordWithoutBraces() {
+      $this->parse('require "functions.inc";');
+    }
+
+    /**
+     * Test "echo" keyword is not supported without braces
+     *
+     * @see   php://echo
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function echoKeywordWithoutBraces() {
+      $this->parse('echo "Hello";');
+    }
+
+    /**
+     * Test alternative syntax for control structures are not supported
+     *
+     * @see   php://control-structures.alternative-syntax
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function alternativeIf() {
+      $this->parse('if ($a): $b++; endif;');
+    }
+
+    /**
+     * Test alternative syntax for control structures are not supported
+     *
+     * @see   php://control-structures.alternative-syntax
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function alternativeWhile() {
+      $this->parse('while ($a > 0): $a--; endwhile;');
+    }
+
+    /**
+     * Test alternative syntax for control structures are not supported
+     *
+     * @see   php://control-structures.alternative-syntax
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function alternativeFor() {
+      $this->parse('for ($i= 0; $i < 4; $i++): $b--; endfor;');
+    }
+
+    /**
+     * Test alternative syntax for control structures are not supported
+     *
+     * @see   php://control-structures.alternative-syntax
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function alternativeForeach() {
+      $this->parse('foreach ($a in $list): $b--; endforeach;');
+    }
+
+    /**
+     * Test silence operator is not supported
+     *
+     * @see   php://language.operators.errorcontrol
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function silenceOperator() {
+      $this->parse('$a= @$b;');
+    }
+
+    /**
+     * Test execution operator is not supported
+     *
+     * @see   php://language.operators.execution
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function executionOperator() {
+      $this->parse('$a= `ls -al`;');
+    }
+
+    /**
+     * Test inline HTML is not supported
+     *
+     * @see   php://language.basic-syntax.phpmode
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function inlineHTML() {
+      $this->parse('?>HTML<?php');
+    }
+
+    /**
+     * Test hash (#) comment is not supported
+     *
+     * @see   php://language.basic-syntax.comments
+     */
+    #[@test, @expect('lang.FormatException')]
+    public function hashComment() {
+      $this->parse('# $a= 1;');
+    }
+  }
+?>
