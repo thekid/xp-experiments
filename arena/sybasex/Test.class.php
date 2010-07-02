75f776750a8a9cd417057435e5916242e7da36c8
kiesel <kiesel@svn.xp-framework.net>
2010-07-02 11:11:30 +0000

    - set host, user and password as commandline arguments
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12370 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/sybasex/Test.class.php
+++ arena/sybasex/Test.class.php
@@ -9,14 +9,49 @@
   );
 
   class Test extends Command {
+    protected
+      $host = NULL,
+      $user = NULL,
+      $pass = NULL;
+
+    /**
+     * Set host to connect to
+     *
+     * @param   string host
+     */
+    #[@arg]
+    public function setHost($host) {
+      $this->host= $host;
+    }
+
+    /**
+     * Set username
+     *
+     * @param   string user
+     */
+    #[@arg]
+    public function setUser($user) {
+      $this->user= $user;
+    }
+
+    /**
+     * Set password
+     *
+     * @param   string pass
+     */
+    #[@arg]
+    public function setPass($pass) {
+      $this->pass= $pass;
+    }
+
     public function run() {
       $protocol= new SybasexProtocol();
       $protocol->setTrace(Logger::getInstance()->getCategory()->withAppender(new ColoredConsoleAppender()));
 
-      $socket= new Socket('localhost', 1999);
+      $socket= new Socket($this->host, 1999);
       $socket->setTimeout(2);
 
-      $protocol->connect($socket);
+      $protocol->connect($socket, $this->user, $this->pass);
     }
   }
 
