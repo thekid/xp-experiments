4f1a506681cc6177eb15181bc5de4e1e8f4e9a80
kiesel <kiesel@svn.xp-framework.net>
2010-07-06 08:03:09 +0000

    - remove dead code
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12410 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/sybasex/src/rdbms/sybasex/token/TdsRowResultToken.class.php
+++ arena/sybasex/src/rdbms/sybasex/token/TdsRowResultToken.class.php
@@ -11,34 +11,6 @@
 
         $record[]= $column->getType()->fromWire($this->data, $column);
         continue;
-
-        $size= NULL;
-        switch ($column['type']->size()) {
-          case 5: {
-            $size= $this->data->readLong();
-            break;
-          }
-
-          case 2: {
-            $size= $this->data->readSmallInt();
-            break;
-          }
-
-          case 1: {
-            $size= $this->data->readByte();
-            break;
-          }
-
-          case 0: {
-            // TODO: Why?
-            $size= $column['type']->fixedSize();
-            break;
-          }
-
-          default: {
-            throw new SybasexRuntimeException('Invalid column size when reading result row: '.$column['type']->size());
-          }
-        }
       }
       
       $this->cat && $this->cat->debug('Read row:', $record);
