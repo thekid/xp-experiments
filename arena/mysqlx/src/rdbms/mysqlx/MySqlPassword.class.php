9def33383e0e709b2e689dac59989848a7f4ce8b
Timm Friebe <thekid@thekid.de>
2010-08-03 12:59:16 +0000

    - Add support for 4.0 # Step 2, password scrambling implemented, various other differences missing
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12495 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/mysqlx/src/rdbms/mysqlx/MySqlPassword.class.php
+++ arena/mysqlx/src/rdbms/mysqlx/MySqlPassword.class.php
@@ -4,7 +4,7 @@
  * $Id$ 
  */
 
-  uses('lang.Enum');
+  uses('lang.Enum', 'math.BigInt');
 
   /**
    * (Insert class' description here)
@@ -18,15 +18,49 @@
       $PROTOCOL_41= NULL;
     
     static function __static() {
-      self::$PROTOCOL_40= newinstance(__CLASS__, array(0, '$PROTOCOL_40'), '{
+      self::$PROTOCOL_40= newinstance(__CLASS__, array(0, 'PROTOCOL_40'), '{
         static function __static() { }
+        
+        public static function hash($in) {
+          $nr= new BigInt(1345345333);
+          $nr2= new BigInt(0x12345671);
+          $add= new BigInt(7);
+
+          for ($i= 0, $s= strlen($in); $i < $s; $i++) {
+            $ord= ord($in{$i});
+            if (0x20 === $ord || 0x09 === $ord) continue;
+            $value= $nr->bitwiseAnd(new BigInt(63))->add($add)->multiply(new BigInt($ord))->add($nr->multiply(new BigInt(0x100)));
+            $nr= $nr->bitwiseXor($value);
+            $nr2= $nr2->multiply(new BigInt(0x100))->bitwiseXor($nr)->add($nr2);
+            $add= $add->add(new BigInt($ord));
+          }
+          return array($nr->bitwiseAnd(new BigInt(0x7FFFFFFF)), $nr2->bitwiseAnd(new BigInt(0x7FFFFFFF)));
+        }
         
         public function scramble($password, $message) {
           if ("" === $password || NULL === $password) return "";
-          raise("lang.MethodNotImplementedException", "scramble() not yet supported for 4.0");
+
+          $hp= self::hash($password);
+          $hm= self::hash($message);
+          $SEED_MAX= new BigInt(0x3FFFFFFF);
+
+          $seed1= $hp[0]->bitwiseXor($hm[0])->modulo($SEED_MAX);
+          $seed2= $hp[1]->bitwiseXor($hm[1])->modulo($SEED_MAX);
+          $to= "";
+          for ($i= 0, $s= strlen($message); $i < $s; $i++) {
+            $seed1= $seed1->multiply(new BigInt(3))->add($seed2)->modulo($SEED_MAX);
+            $seed2= $seed1->add($seed2)->add(new BigInt(33))->modulo($SEED_MAX);
+            $rnd= $seed1->divide($SEED_MAX)->multiply(new BigInt(31));
+            $to.= chr($rnd->intValue() + 64);
+          }
+          $seed1= $seed1->multiply(new BigInt(3))->add($seed2)->modulo($SEED_MAX);
+          $seed2= $seed1->add($seed2)->add(new BigInt(33))->modulo($SEED_MAX);
+          $rnd= $seed1->divide($SEED_MAX)->multiply(new BigInt(31));
+
+          return $to ^ str_repeat(chr($rnd->intValue()), strlen($message));
         }
       }');
-      self::$PROTOCOL_41= newinstance(__CLASS__, array(1, '$PROTOCOL_41'), '{
+      self::$PROTOCOL_41= newinstance(__CLASS__, array(1, 'PROTOCOL_41'), '{
         static function __static() { }
         public function scramble($password, $message) {
           if ("" === $password || NULL === $password) return "";
