ed02ab022ea6d3a7ec3830a84f1426d280baef29
Timm Friebe <thekid@thekid.de>
2011-01-12 10:24:18 +0000

    - Add getName() - Fix invoke() for instance methods
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12623 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- people/friebe/jsxp/lang/reflect/Method.js
+++ people/friebe/jsxp/lang/reflect/Method.js
@@ -10,11 +10,22 @@ function Method(clazz, name, modifiers) {
 
 Method.prototype= new Object();
 
+Method.prototype.name = '';
+
+Method.prototype.getName = function() {
+  return this.name;
+}
+
 Method.prototype.toString = function() {
   return 'lang.reflect.Method<' + (this.modifiers & 1 ? 'static ' : '') + this.clazz.name + '::' + this.name + '>';
 }
 
 Method.prototype.invoke = function(obj, args) {
-  return $xp[this.clazz.name][this.name].apply(obj, args);
+  var handle = $xp[this.clazz.reflect];
+  if (typeof(handle.prototype[this.name]) !== 'undefined') {
+    return handle.prototype[this.name].apply(obj, args);
+  } else {
+    return handle[this.name].apply(obj, args);
+  }
 }
 // }}}
