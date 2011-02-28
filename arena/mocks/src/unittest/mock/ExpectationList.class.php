7d3274a3fa57907ff612bf453247cafe8effa784
ilankin <ilankin@svn.xp-framework.net>
2011-02-28 10:38:49 +0000

    * added add and getNext methods
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12678 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/mocks/src/unittest/mock/ExpectationList.class.php
+++ arena/mocks/src/unittest/mock/ExpectationList.class.php
@@ -3,6 +3,7 @@
  *
  * $Id$
  */
+  uses('lang.IllegalArgumentException');
 
 /**
  * A stateful list for expectations.
@@ -10,5 +11,43 @@
  * @purpose 
  */
   class ExpectationList extends Object {
+    private 
+      $list= null,
+      $current=0;
+    
+      /**
+       * Constructor      
+       */
+      public function __construct() {
+        $this->list=new Vector();
+      }
 
+
+      /**
+       * Adds an expectation.
+       * 
+       * @param unittest.mock.Expectation expectation    
+       */
+      public function add($expectation) {
+        if(!($expectation instanceof Expectation))
+          throw new IllegalArgumentException("Expectation expected.");
+        
+        $this->list->add($expectation);
+      }
+
+
+      /**
+       * Returns the next expectation or null if no expectations left.
+       * 
+       * @return unittest.mock.Expectation  
+       */
+      public function getNext() {
+        if($this->current >= $this->list->size())
+          return null;
+        
+        return $this->list->get($this->current++);
+      }
+
+    
+    
 }
--- arena/mocks/tests/net/xp_framework/unittest/tests/mock/ExpectationListTest.class.php
+++ arena/mocks/tests/net/xp_framework/unittest/tests/mock/ExpectationListTest.class.php
@@ -4,7 +4,8 @@
  * $Id$
  */
 
-  uses('unittest.mock.ExpectationList');
+  uses('unittest.mock.ExpectationList',
+       'unittest.mock.Expectation');
   
 /**
  * Test cases for the ExpectationList class
@@ -30,5 +31,66 @@
       new ExpectationList();
     }
 
+    /**
+     * getNext should exist.          
+     */
+    #[@test]
+    public function canCallGetNext() {
+      $this->sut->getNext();
+    }
+    
+    /**
+     * getNext should return null after initialization.                  
+     */
+    #[@test]
+    public function getNext_returnNullByDefault() {
+      $this->assertNull($this->sut->getNext());
+    }
+    
+    /**
+     * add method should exist.            
+     */
+    #[@test]
+    public function canAddExpectation() {
+      $this->sut->add(new Expectation());
+    }
+    
+    /**
+     * Added expectations should be returned by getNext.
+     */
+    #[@test]
+    public function getNextReturnsAddedExpectation() {
+      $expect=new Expectation();
+      $this->sut->add($expect);
+      
+      $this->assertEquals($expect, $this->sut->getNext());
+    }
+    
+    /**
+     * If no more expectations left, getNext should return null.
+     */
+    #[@test]
+    public function getNextReturnsNullIfNoMoreElements() {
+      $expect=new Expectation();
+      $this->sut->add($expect);
+      
+      $this->assertEquals($expect, $this->sut->getNext());
+      $this->assertNull($this->sut->getNext());
+    }
+    
+    /**
+     * Null shall never be added.
+     */
+    #[@test, @expect('lang.IllegalArgumentException')]
+    public function cannotAddNull() {
+      $this->sut->add(null);
+    }
+    
+    /**
+     * Another object shall never be added.
+     */
+    #[@test, @expect('lang.IllegalArgumentException')]
+    public function cannotAddObjects() {
+      $this->sut->add(new Object());
+    }
   }
-?>
