a108cd69bcd8a49d7e07cff8b241d20b9683db63
robert <robert@svn.xp-framework.net>
2011-04-15 12:09:54 +0000

    - initial revision
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12803 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ people/robert/netbeans/class-templates/xp_unittest_class.php
@@ -0,0 +1,35 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'unittest.TestCase',
+    'path.to.${name}'
+  );
+
+  /**
+   * @TODO: Description and path of ${name}
+   *
+   * @purpose  unittest of ${name}
+   * @see      xp://path.to.${name}
+   */
+  class ${name} extends TestCase {
+
+    /**
+     * Set up this test
+     *
+     */
+    public function setUp() {
+      // Enter code here or delete this function
+    }
+
+    /**
+     * Tear down the this test case.
+     * 
+     */
+    public function tearDown() {
+      // Enter code here or delete this function
+    }
+  }
