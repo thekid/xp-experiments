b1445bc6e66133777652480417c7d65e4e71cec2
ilankin <ilankin@svn.xp-framework.net>
2011-04-18 09:03:38 +0000

    * added cluster test and load test
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/experiments@12806 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- arena/mq-tests/src/test/php/org/oneandone/mq/unittest/BasicTests.class.php
+++ arena/mq-tests/src/test/php/org/oneandone/mq/unittest/BasicTests.class.php
@@ -24,34 +24,48 @@
      *
      */
     public function setUp() {
-      $this->sender= new StompConnection('sharp.i.schlund.de', 61613);
+      $this->sender= new StompConnection('vm720.development.lan', 61613);
       $this->sender->setTrace(Logger::getInstance()->getCategory());
       $this->sender->connect('system', 'manager');
       
-      $this->receiver= new StompConnection('sharp.i.schlund.de', 61613);
+      $this->receiver= new StompConnection('vm721.development.lan', 61613);
       $this->receiver->setTrace(Logger::getInstance()->getCategory());
       $this->receiver->connect('system', 'manager');
       $this->receiver->subscribe($this->queue);
+
     }
 
-    /**
 
-     */
     #[@test]
     public function delay_headers_should_delay_message() {
       $delay= 3000;
       $headers= array();
       $headers['AMQ_SCHEDULED_DELAY']= $delay;
-      $msg= 'whaaaazuuup!? - '.uniqid();
+      $headers['persistent']= 'true';
+      $msg= 'foooup!? - '.uniqid();
       $this->sender->send($this->queue, $msg, $headers);
       $sendtime= Date::now();
 
-      $message= $this->receiver->receive($delay/1000.0 + 1); //wait for it
+      $message= $this->receiver->receive($delay/1000.0 + 5); //wait for it
       $rcvtime= Date::now();
 
       $this->assertTrue(($rcvtime->getTime() - $sendtime->getTime() + 1) >= $delay/1000.0); //at least $deleay-1 secs
       $this->assertEquals($msg, $message->getBody());
     }
 
-  
+    #[@test]
+    public function make_many_messages() {
+      $count= 500;
+      $delay=50000;
+
+      $headers= array();
+     // $headers['AMQ_SCHEDULED_DELAY']= $delay;
+      $headers['persistent']='true';
+      $msg= 'whaaaazuuup!? - '.uniqid();
+      for($i=0; $i<$count; ++$i) {
+        $this->sender->send($this->queue, $msg, $headers);
+      }
+    }
+
+
   }
