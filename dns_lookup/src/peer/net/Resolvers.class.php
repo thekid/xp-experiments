e5c9252094ef0296008d7bb237cbd3bc58a55130
Timm Friebe <thekid@thekid.de>
2011-04-24 13:01:31 +0200

    - Implement parsing /etc/resolv.conf

--- dns_lookup/src/peer/net/Resolvers.class.php
+++ dns_lookup/src/peer/net/Resolvers.class.php
@@ -21,7 +21,7 @@
     
       // Fetch DNS server(s)
       $nameservers= array();
-      if (1) {
+      if (strncasecmp(PHP_OS, 'Win', 3) === 0) {
         $c= new COM('winmgmts://./root/cimv2');
         $q= $c->ExecQuery('select DNSServerSearchOrder from Win32_NetworkAdapterConfiguration where IPEnabled = true');
         foreach ($q as $result) {
@@ -29,6 +29,14 @@
             $nameservers[]= $server;
           }
         }
+      } else if (file_exists('/etc/resolv.conf')) {
+        foreach (file('/etc/resolv.conf') as $line) {
+          if (strncmp($line, 'nameserver', 10) === 0) {
+            $nameservers[]= rtrim(substr($line, 11));
+          }
+        }
+      } else {
+        throw new IllegalStateException('No system resolvers could be determined');
       }
       
       return new DefaultResolver($nameservers[0]);
