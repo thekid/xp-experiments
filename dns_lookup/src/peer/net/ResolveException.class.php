b94c30d673d1686eceb6545a9c93ef798f07eb2c
Timm Friebe <thekid@thekid.de>
2011-06-25 16:24:01 +0200

    - Initial release

---
+++ dns_lookup/src/peer/net/ResolveException.class.php
@@ -0,0 +1,14 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Indicates an error occured during processing
+   *
+   */
+  class ResolveException extends Throwable {
+    
+  }
+?>
