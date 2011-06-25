919d87cd4a15b8a6b1a6f29d6254146e871b3be7
Timm Friebe <thekid@thekid.de>
2011-06-25 15:29:17 +0200

    - Refactor strings in request message to Query instance

---
+++ dns_lookup/src/peer/net/Query.class.php
@@ -0,0 +1,59 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  $package= 'peer.net';
+
+  uses('peer.net.Record', 'peer.net.QType');
+
+  /**
+   * Query record
+   *
+   */
+  class peer·net·Query extends peer·net·Record {
+    protected $qtype = NULL;
+    protected $qclass = 0;
+   
+    /**
+     * Creates a new record
+     *
+     * @param   string name
+     * @param   peer.net.QType qtype
+     * @param   int qclass
+     */
+    public function __construct($name, $qtype, $qclass) {
+      parent::__construct($name, 0);
+      $this->qtype= $qtype;
+      $this->qclass= $qclass;
+    }
+
+    /**
+     * Gets type
+     *
+     * @return  peer.net.QType
+     */
+    public function getQType() {
+      return $this->qtype;
+    }
+
+    /**
+     * Gets dclass
+     *
+     * @return  int
+     */
+    public function getQClass() {
+      return $this->qclass;
+    }
+
+    /**
+     * Creates a string representation of this record
+     *
+     * @return  string
+     */
+    public function toString() {
+      return $this->getClassName().'('.$this->name.' '.$this->type->toString().' / '.$this->dclass.')';
+    }
+  }
+?>
