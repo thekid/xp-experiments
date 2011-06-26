de41f34ed6ada9478ad2b267d7a3a64b28b57d4d
Timm Friebe <thekid@thekid.de>
2011-06-26 16:20:45 +0200

    - Add QType tests

---
+++ dns_lookup/src/net/xp_framework/unittest/peer/net/QTypeTest.class.php
@@ -0,0 +1,55 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'peer.net.QType'
+  );
+
+  /**
+   * TestCase
+   *
+   * @see   xp://peer.net.QType
+   */
+  class QTypeTest extends TestCase {
+  
+    /**
+     * Test named() method
+     *
+     */
+    #[@test]
+    public function namedA() {
+      $this->assertEquals(QType::$A, QType::named('A'));
+    }
+
+    /**
+     * Test named() method
+     *
+     */
+    #[@test, @expect('lang.IllegalArgumentException')]
+    public function lowerCaseNamesNotSupported() {
+      QType::named('a');
+    }
+
+    /**
+     * Test named() method
+     *
+     */
+    #[@test, @expect('lang.IllegalArgumentException')]
+    public function emptyName() {
+      QType::named('');
+    }
+
+    /**
+     * Test named() method
+     *
+     */
+    #[@test, @expect('lang.IllegalArgumentException')]
+    public function nonExistantName() {
+      QType::named('AAA');
+    }
+  }
+?>
