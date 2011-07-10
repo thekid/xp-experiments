f1b851398c7f22bd9e7dfc59f5ee01480dd470d3
Timm Friebe <thekid@thekid.de>
2011-07-10 15:59:14 +0200

    - REST API: Initial release

---
+++ rest/src/main/php/webservices/rest/RestParameters.class.php
@@ -0,0 +1,16 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * REST parameter types
+   *
+   * @see   xp://webservices.rest.RestRequest#addParameter
+   */
+  class RestParameters extends Object {
+    const REQUEST = 0;
+    const SEGMENT = 1;
+  }
+?>
