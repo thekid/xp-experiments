cf73cd575a3c9c85a49977e8804910a931568374
Timm Friebe <thekid@thekid.de>
2011-07-10 15:08:40 +0200

    - Implement request method accessors - Implement request resource accessors

--- rest/src/main/php/webservices/rest/RestRequest.class.php
+++ rest/src/main/php/webservices/rest/RestRequest.class.php
@@ -4,11 +4,83 @@
  * $Id$ 
  */
 
+  uses('peer.http.HttpConstants');
+
   /**
-   * (Insert class' description here)
+   * A REST request
    *
    */
   class RestRequest extends Object {
+    protected $resource= '';
+    protected $method= '';
+
+    /**
+     * Creates a new RestRequest instance
+     *
+     * @param   string resource default NULL
+     * @param   string method default HttpConstants::GET
+     */
+    public function __construct($resource= NULL, $method= HttpConstants::GET) {
+      if (NULL !== $resource) $this->setResource($resource);
+      $this->method= $method;
+    }
     
+    /**
+     * Sets resource
+     *
+     * @param   string resource
+     */
+    public function setResource($resource) {
+      $this->resource= $resource;
+    }
+
+    /**
+     * Sets resource
+     *
+     * @param   string resource
+     * @return  webservices.rest.RestRequest
+     */
+    public function withResource($resource) {
+      $this->resource= $resource;
+      return $this;
+    }
+
+    /**
+     * Gets resource
+     *
+     * @return  string resource
+     */
+    public function getResource() {
+      return $this->resource;
+    }
+
+    /**
+     * Sets method
+     *
+     * @param   string method
+     */
+    public function setMethod($method) {
+      $this->method= $method;
+    }
+
+    /**
+     * Sets method
+     *
+     * @param   string method
+     * @return  webservices.rest.RestRequest
+     */
+    public function withMethod($method) {
+      $this->method= $method;
+      return $this;
+    }
+
+    /**
+     * Gets method
+     *
+     * @return  string method
+     */
+    public function getMethod() {
+      return $this->method;
+    }
   }
 ?>
