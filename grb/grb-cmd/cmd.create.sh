f04697ff2554849358f0f613e63b55b03ed07885
Timm Friebe <thekid@thekid.de>
2012-01-21 10:53:54 +0100

    - Support optional second argument which base branch to   base the newly created branch on # E.g. grb create "modules" "xp5_9"

--- grb/grb-cmd/cmd.create.sh
+++ grb/grb-cmd/cmd.create.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 #-grb create: Create a branch
-  BRANCH=$1
+  GRB_BRANCH=$1
+  GRB_BASE=${2-master}
 shift 1 || exit 2
 
-$EXEC git push origin master:refs/heads/$BRANCH
+$EXEC git push origin $GRB_BASE:refs/heads/$GRB_BRANCH
 $EXEC git fetch origin
-$EXEC git branch --track $BRANCH origin/$BRANCH
-$EXEC git checkout $BRANCH
+$EXEC git branch --track $GRB_BRANCH origin/$GRB_BRANCH
+$EXEC git checkout $GRB_BRANCH
