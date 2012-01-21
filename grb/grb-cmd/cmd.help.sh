c2c3f07a5bb7defed6b7c69a52a3e4b6efe5aef5
Timm Friebe <thekid@thekid.de>
2012-01-21 10:53:03 +0100

    - Support more than one command line arg

--- grb/grb-cmd/cmd.help.sh
+++ grb/grb-cmd/cmd.help.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 #-grb help: Context-sensitive help on a certain command
-  COMMAND=$1
+  GRB_COMMAND=$1
 shift 1 || exit 2
 
 EXEC=echo
-if [ -f "$DIRNAME"/cmd.${COMMAND}.sh ]; then
-  grep -A 1 '^#-grb' "$DIRNAME"/cmd.${COMMAND}.sh | sed -e 's/#-grb/* grb/g'
+COMMAND="$DIRNAME"/cmd.${GRB_COMMAND}.sh 
+if [ -f "$COMMAND" ]; then
+  grep '^#-grb' "$COMMAND" | sed -e 's/#-grb/* grb/g'
+  grep '^  GRB' "$COMMAND" | sed -e 's/GRB_//g' | sed -e 's/\-/ || /g'
   echo 
-  . "$DIRNAME"/cmd.${COMMAND}.sh "$@"
+  . "$COMMAND" "$@"
 else
-  "No such command: $COMMAND"
+  "No such command: $GRB_COMMAND"
 fi
 
 exit 3
