643e5ed5872c42a5881a06e64a2a5afbeeffc7d7
Timm Friebe <thekid@thekid.de>
2012-03-11 14:58:06 +0100

    - Simplify source inside lang.reflect.Method

--- jsxp/lang/reflect/Method.js
+++ jsxp/lang/reflect/Method.js
@@ -1,8 +1,15 @@
+uses('lang.ElementNotFoundException');
+
 // {{{ Method
 lang.reflect.Method = function(clazz, name, modifiers) {
   {
     this.__class = 'lang.reflect.Method';
     this.clazz = clazz;
+    if (typeof(clazz.reflect.prototype[name]) !== 'undefined') {
+      this.reflect = clazz.reflect.prototype[name];
+    } else {
+      this.reflect = clazz.reflect[name];
+    }
     this.name = name;
     this.modifiers = modifiers;
   }
@@ -21,40 +28,21 @@ lang.reflect.Method.prototype.toString = function() {
 }
 
 lang.reflect.Method.prototype.invoke = function(obj, args) {
-  var handle = this.clazz.reflect;
-  if (typeof(handle.prototype[this.name]) !== 'undefined') {
-    return handle.prototype[this.name].apply(obj, args);
-  } else {
-    return handle[this.name].apply(obj, args);
-  }
+  return this.reflect.apply(obj, args);
 }
 
 lang.reflect.Method.prototype.hasAnnotations = function(name) {
-  var handle = this.clazz.reflect;
-  if (typeof(handle.prototype[this.name]) !== 'undefined') {
-    return typeof(handle.prototype[this.name]['@']) !== 'undefined';
-  } else {
-    return typeof(handle[this.name]['@']) !== 'undefined';
-  }
+  return typeof(this.reflect['@']) !== 'undefined';
 }
 
 lang.reflect.Method.prototype.hasAnnotation = function(name) {
-  if (!this.hasAnnotations()) return false;
-  var handle = this.clazz.reflect;
-  if (typeof(handle.prototype[this.name]) !== 'undefined') {
-    return typeof(handle.prototype[this.name]['@'][name]) !== 'undefined';
-  } else {
-    return typeof(handle[this.name]['@'][name]) !== 'undefined';
-  }
+  return this.hasAnnotations() && typeof(this.reflect['@'][name]) !== 'undefined';
 }
 
 lang.reflect.Method.prototype.getAnnotation = function(name) {
-  if (!this.hasAnnotation(name)) return false;
-  var handle = this.clazz.reflect;
-  if (typeof(handle.prototype[this.name]) !== 'undefined') {
-    return handle.prototype[this.name]['@'][name];
-  } else {
-    return handle[this.name]['@'][name];
+  if (!this.hasAnnotation(name)) {
+    throw new lang.ElementNotFoundException('No such annotation "' + name + '"');
   }
+  return this.reflect['@'][name];
 }
 // }}}
