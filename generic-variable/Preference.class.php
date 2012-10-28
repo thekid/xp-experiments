a090cad7391aca1870d228b2b17e1626b1ef436a
Timm Friebe <thekid@thekid.de>
2012-10-28 10:55:48 +0100

    - Add Preference::all()

--- generic-variable/Preference.class.php
+++ generic-variable/Preference.class.php
@@ -1,19 +1,27 @@
 <?php
   #[@generic(self= 'T')]
   class Preference extends Object {
-  	protected $list= array();
+    protected $list= array();
 
-  	public function __construct($str) {
-  	  $this->list= explode(',', $str);
-  	}
+    public function __construct($str) {
+      $this->list= explode(',', $str);
+    }
 
-  	#[@generic(return= 'T')]
-  	public function match(array $supported) {
-  	  /**/ $__c= get_class($this); $T= substr($__c, strpos($__c, '··')+ 2); /**/
-  	  foreach ($this->list as $value) {
-  	  	if (in_array($value, $supported)) return new $T($value);
-  	  }
-  	  return NULL;
-  	}
+    #[@generic(return= 'T')]
+    public function match(array $supported) {
+      foreach ($this->list as $value) {
+        if (in_array($value, $supported)) return new $T($value);
+      }
+      return NULL;
+    }
+
+    #[@generic(return= 'T[]')]
+    public function all() {
+      $r= array();
+      foreach ($this->list as $value) {
+        $r[]= new $T($value);
+      }
+      return $r;
+    }
   }
 ?>
--- generic-variable/PreferenceTest.class.php
+++ generic-variable/PreferenceTest.class.php
@@ -3,13 +3,22 @@
 
   class PreferenceTest extends TestCase {
 
-  	#[@test]
-  	public function content_type() {
+    #[@test]
+    public function match() {
       $fixture= create('new Preference<ContentType>', 'text/html,text/xml');
       $this->assertEquals(
-      	new ContentType('text/html'), 
-      	$fixture->match(array('text/html'))
+        new ContentType('text/html'), 
+        $fixture->match(array('text/html'))
       );
-  	}
+    }
+
+    #[@test]
+    public function all() {
+      $fixture= create('new Preference<ContentType>', 'text/html,text/xml');
+      $this->assertEquals(
+        array(new ContentType('text/html'), new ContentType('text/xml')),
+        $fixture->all()
+      );
+    }
   }
 ?>
