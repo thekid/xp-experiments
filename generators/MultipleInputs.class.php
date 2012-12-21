a88037712fc2db5a64e0e4420b769ba5b5d86953
Timm Friebe <thekid@thekid.de>
2012-12-21 12:12:52 +0100

    - Initial release

---
+++ generators/MultipleInputs.class.php
@@ -0,0 +1,38 @@
+<?php
+  /**
+   * Processes multiple inputs in parallel
+   */
+  class MultipleInputs extends Object {
+
+    protected static function numbers($start, $max) {
+      for ($i= $start; $i < $max; $i+= 2) {
+        yield $i;
+      }
+    }
+
+    protected static function odd($max) {
+      return self::numbers(1, $max);
+    }
+
+    protected static function even($max) {
+      return self::numbers(0, $max);
+    }
+
+    protected static function all() {
+      $generators= func_get_args();
+      do {
+        foreach ($generators as $i => $generator) {
+          yield $generator->current();
+          $generator->next();
+          if (!$generator->valid()) unset($generators[$i]);
+        }
+      } while ($generators);
+    }
+
+  	public static function main(array $args) {
+      foreach (self::all(self::even(10), self::odd(10)) as $number) {
+        Console::writeLine($number); 
+      }
+    }
+  }
+?>
