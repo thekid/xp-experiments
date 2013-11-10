506f82686b0c921064a592959688f705a77cbb69
Timm Friebe <thekid@thekid.de>
2013-11-10 20:52:55 +0100

    Pass $this as $self

--- newinstance-closures/Test.class.php
+++ newinstance-closures/Test.class.php
@@ -1,49 +1,15 @@
 <?php
-function newinstance_($intf, $args, $def) {
-  static $uniq= 0;
-
-  $uniq++;
-  $src= 'class NewInstance_'.$uniq.' implements '.\lang\XPClass::forName($intf)->literal().' { static $def; ';
-  foreach ($def as $name => $function) {
-
-    // Create pass
-    $r= new ReflectionFunction($function);
-    $pass= '';
-    foreach ($r->getParameters() as $param) {
-      $pass.= ', '.$param->getName();
-    }
-
-    // Create method
-    $src.= 'function '.$name.'('.substr($pass, 2).') {
-      $f= self::$def["'.$name.'"];
-      return $f('.('' === $pass ? '' : ', '.substr($pass, 2)).');
-    }';
-  }
-  $src.= '}';
-  eval($src);
-
-  // Instantiate
-  $c= new ReflectionClass('NewInstance_'.$uniq);
-  $c->setStaticPropertyValue('def', $def);
-  return $c->newInstanceArgs($args);
-}
+require('newinstance_.php');
 
 class Test extends \lang\Object {
 
   public static function main($args) {
     $run= newinstance_('lang.Runnable', [], [
-      'run' => function() {
-        //Console::writeLine('Running...');
+      'run' => function($self) {
+        Console::writeLine('It works - inside ', $self->getClassName());
       }
     ]);
 
-    Console::writeLine('Running...');
-    $times= 1000000;
-    $timer= \util\profiling\Timer::measure(function() use($run, $times) {
-      for ($i= 0; $i < $times; $i++) {
-        $run->run();
-      }
-    });
-    Console::writeLinef('%.3f seconds for %d run(s)', $timer->elapsedTime(), $times);
+    $run->run();
   }
 }
