e7c94d4571f81359b08553c85754dceca4e75bbb
Timm Friebe <thekid@thekid.de>
2014-03-24 22:42:58 +0100

    Hide ugly "extends ClassLoader" hack inside inner class

--- java8/intf-inst/InterfaceInstance.java
+++ java8/intf-inst/InterfaceInstance.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayOutputStream;
 
 public class InterfaceInstance extends ClassLoader {
     private Class<?> base;
+    private Loader loader = new Loader();
     private static final AtomicLong uniq = new AtomicLong();
 
     public InterfaceInstance(Class<?> base) throws IllegalArgumentException {
@@ -16,32 +17,35 @@ public class InterfaceInstance extends ClassLoader {
         this.base = base;
     }
 
-    public Class findClass(String name) {
-        final byte[] definition = loadClassData(name);
-        return defineClass(name, definition, 0, definition.length);
-    }
+    private class Loader extends ClassLoader {
 
-    private byte[] loadClassData(String name) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        try {
-            ClassWriter writer = new ClassWriter(new ClassInfo(
-                name,
-                "java.lang.Object",
-                new String[] { this.base.getName() }
-            ));
-            writer.writeTo(bytes);
-            final byte[] definition = bytes.toByteArray();
+        public Class findClass(String name) {
+            final byte[] definition = loadClassData(name);
+            return defineClass(name, definition, 0, definition.length);
+        }
 
-            // DEBUG
-            // java.nio.file.Files.write(java.nio.file.Paths.get(name + ".class"), definition);
+        private byte[] loadClassData(String name) {
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            try {
+                ClassWriter writer = new ClassWriter(new ClassInfo(
+                    name,
+                    "java.lang.Object",
+                    new String[] { InterfaceInstance.this.base.getName() }
+                ));
+                writer.writeTo(bytes);
+                final byte[] definition = bytes.toByteArray();
 
-            return definition;
-        } catch (final Exception e) {
-            throw new RuntimeException("Cannot load " + name, e);
+                // DEBUG
+                // java.nio.file.Files.write(java.nio.file.Paths.get(name + ".class"), definition);
+
+                return definition;
+            } catch (final Exception e) {
+                throw new RuntimeException("Cannot load " + name, e);
+            }
         }
     }
 
     public Class<?> defineClass() throws ClassNotFoundException {
-        return super.loadClass(this.base.getName() + "$Proxy$" + uniq.getAndIncrement());
+        return loader.loadClass(base.getName() + "$Proxy$" + uniq.getAndIncrement());
     }
 }
